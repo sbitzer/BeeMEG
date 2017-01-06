971e44c125bb341ccb2ab0a97216ef5e40a50f75
Sebastian Bitzer <sebastian.bitzer@tu-dresden.de>
2017-01-06 18:57:14 +0100

    ran logistic regression on choices across all subjects
    As my initial tests with single subjects indicated, there appears to be a clear
    effect in all subjects. Even the subject with the highest p-value of 2.15e-06
    exhibits a clear increase of fraction correct choices with increasing support
    for correct choice bin.
    
    I also see effects in RT, but they don't appear to be as strong, or easily
    interpretable as the effects in choice.
    
    Given that we see this strong effect in choice, does that mean that subjects
    made a response immediately after the 5th dot in many trials? Not necessarily,
    as the 5th dot may have influenced also later choices.

--- analyse_5thdot_behaviour.py
+++ analyse_5thdot_behaviour.py
@@ -12,69 +12,84 @@ import helpers
 import seaborn as sns
 from statsmodels.formula.api import logit
 
-sub = 15
-
-respRT = helpers.load_subject_data(sub)
 dotpos = helpers.load_dots(dotdir='data')
 maxrt = dotpos.shape[0] * helpers.dotdt
 trial_info = helpers.get_5th_dot_infos(dotpos)
 
-respRT_ext = pd.concat([respRT, trial_info], axis=1)
+# use "None" for all subjects; for a single subject (e.g. "[24]") plots will
+# be shown
+subjects = [24]
 
+if subjects is None:
+    subjects = helpers.find_available_subjects()
+subjects = np.sort(np.array(subjects))
+S = subjects.size
+    
+subject_info = pd.DataFrame([], index=pd.Index(subjects, name='subject'))
 
-#%% check whether subject responded correctly according to first 4 dots
-respRT_ext['is_correct'] = (respRT_ext.correct_4th == respRT_ext.response).astype(int)
-
-
-#%% define penalty that is 0 for an immediate correct response, maximal for an
-#   immediate wrong response and is continuous in between
-
-# for correct choices, the penalty is equal to the RT
-ind = respRT_ext.response == respRT_ext.correct_4th
-respRT_ext.loc[ind, 'penalty'] = respRT_ext.loc[ind, 'RT']
-
-# for wrong choices, the penalty is 2*maxrt - RT
-ind = respRT_ext.response == -respRT_ext.correct_4th
-respRT_ext.loc[ind, 'penalty'] = 2*maxrt - respRT_ext.loc[ind, 'RT']
-
-# timed-out responses have intermediate penalty
-ind = respRT_ext.response == 0
-respRT_ext.loc[ind, 'penalty'] = maxrt
-
-
-#%% check correlation
-sns.jointplot(x='support_correct', y='penalty', data=respRT_ext)
-
-
-#%% compute mean penalty, fraction correct and median RT in 5th dot bins
-B = respRT_ext['support_correct_bin'].unique().size
-binpenalty = np.zeros(B)
-frac_correct = np.zeros(B)
-medianRT = np.zeros(B)
-
-for i in range(B):
-    binind = respRT_ext['support_correct_bin']==i+1
-    binpenalty[i] = respRT_ext.loc[binind, 'penalty'].mean()
-    frac_correct[i] = np.mean(respRT_ext.loc[binind, 'correct_4th'] == 
-                              respRT_ext.loc[binind, 'response'])
-    medianRT[i] = respRT_ext.loc[binind, 'RT'].median()
-
-sns.plt.figure()
-sns.plt.plot(binpenalty)
-sns.plt.xlabel('support for correct choice')
-sns.plt.ylabel('mean penalty')
-
-sns.plt.figure()
-sns.plt.plot(frac_correct)
-sns.plt.xlabel('support for correct choice')
-sns.plt.ylabel('fraction correct')
-
-sns.plt.figure()
-sns.plt.plot(medianRT)
-sns.plt.xlabel('support for correct choice')
-sns.plt.ylabel('median RT')
-
-
-#%% compute logitstic regression
-logitres = logit('is_correct ~ support_correct', respRT_ext).fit()
-print(logitres.summary())
+for si, sub in enumerate(subjects):
+    respRT = helpers.load_subject_data(sub)
+    respRT_ext = pd.concat([respRT, trial_info], axis=1)
+    
+    #%% check whether subject responded correctly according to first 4 dots
+    respRT_ext['is_correct'] = (respRT_ext.correct_4th == respRT_ext.response).astype(int)
+    
+    
+    #%% define penalty that is 0 for an immediate correct response, maximal for an
+    #   immediate wrong response and is continuous in between
+    
+    # for correct choices, the penalty is equal to the RT
+    ind = respRT_ext.response == respRT_ext.correct_4th
+    respRT_ext.loc[ind, 'penalty'] = respRT_ext.loc[ind, 'RT']
+    
+    # for wrong choices, the penalty is 2*maxrt - RT
+    ind = respRT_ext.response == -respRT_ext.correct_4th
+    respRT_ext.loc[ind, 'penalty'] = 2*maxrt - respRT_ext.loc[ind, 'RT']
+    
+    # timed-out responses have intermediate penalty
+    ind = respRT_ext.response == 0
+    respRT_ext.loc[ind, 'penalty'] = maxrt
+    
+    
+    #%% check correlation
+    if S == 1:
+        sns.jointplot(x='support_correct', y='penalty', data=respRT_ext)
+    
+    
+    #%% compute mean penalty, fraction correct and median RT in 5th dot bins
+    B = respRT_ext['support_correct_bin'].unique().size
+    binpenalty = np.zeros(B)
+    frac_correct = np.zeros(B)
+    medianRT = np.zeros(B)
+    
+    for i in range(B):
+        binind = respRT_ext['support_correct_bin']==i+1
+        binpenalty[i] = respRT_ext.loc[binind, 'penalty'].mean()
+        frac_correct[i] = np.mean(respRT_ext.loc[binind, 'correct_4th'] == 
+                                  respRT_ext.loc[binind, 'response'])
+        medianRT[i] = respRT_ext.loc[binind, 'RT'].median()
+    
+    if S == 1:
+        sns.plt.figure()
+        sns.plt.plot(binpenalty)
+        sns.plt.xlabel('support for correct choice')
+        sns.plt.ylabel('mean penalty')
+        
+        sns.plt.figure()
+        sns.plt.plot(frac_correct)
+        sns.plt.xlabel('support for correct choice')
+        sns.plt.ylabel('fraction correct')
+        
+        sns.plt.figure()
+        sns.plt.plot(medianRT)
+        sns.plt.xlabel('support for correct choice')
+        sns.plt.ylabel('median RT')
+    
+    
+    #%% compute logitstic regression
+    logitres = logit('is_correct ~ support_correct', respRT_ext).fit()
+    subject_info.loc[sub, 'logit_intercept'] = logitres.params.Intercept
+    subject_info.loc[sub, 'logit_coef'] = logitres.params.support_correct
+    subject_info.loc[sub, 'logit_p'] = logitres.llr_pvalue
+    if S == 1:
+        print(logitres.summary())
