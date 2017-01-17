b48d8c68dcae23dd6617fce217be988276700241
Sebastian Bitzer <sebastian.bitzer@tu-dresden.de>
2017-01-17 14:18:42 +0100

    run cluster-level permutation analysis on all subjects
    I still wondered how it could be that almost 80% of maximum clusters identified
    in permuted data were bigger than the largest cluster in the original data. But
    this can simply happen because of chance. In fact, under the null hypothesis
    that the data only has a random relationship with my predictor the p-value
    should have a uniform distribution across different data sets. So, if there is
    no relationship you should expect to see all kinds of p-values and 80% is just
    as plausible as 10%.
    
    Therefore, I decided to check the distribution of p-values across data sets
    (subjects). Specifically, I ran the cluster-level permutation analysis for all
    subjects and stored the p-value of the maximum cluster in the data. If the
    distribution over these p-values was uniform, there would be no relationship of
    the support_correct predictor in my data. If the distribution was skewed towards
    1, I would suspect an issue with the analysis and if the distribution was skewed
    towards 0, I would interpret that as evidence that there is some evidence for
    the relation between support_correct and MEG-signal in the data.
    
    Results (p-values and identified clusters) are stored in
    
    inf_results/clusres_5thdot_meg_201701171125.npz
    
    The distribution of p-values is slightly skewed towards 0 with a median of 0.32.
    The lowest p-values: 0.005, 0.008, 0.023, 0.024, 0.047 for subjects 34, 8, 7,
    32, 18, respectively. So there is very weak, but existing evidence for a
    relationship between support_correct and MEG-signal across subjects.
    
    Technical issues
    ----------------
    - Hame somehow forgot to put the raw fif-files for subject 15 into
      proni/BeeMEG/MEG/Raw/bm15a
    - for subjects 14 and 30 there is behavioural data, but no MEG data, because
      Hame excluded their MEG data due to bad channels or eye movement artefacts

--- analyse_Hames_epochs.py
+++ analyse_Hames_epochs.py
@@ -13,20 +13,19 @@ import helpers
 import statsmodels.formula.api as smf
 import pandas as pd
 import seaborn as sns
-import scipy
+import collections
 
 
-#%% prepare epochs
-sub = 5
+#%%
+subjects = None
 
-# example raw file of subject 2, block 1
-rawfile = '/home/bitzer/proni/BeeMEG/MEG/Raw/bm02a/bm02a1.fif'
-
-# get MEG-info from raw file (for channel layout and names)
-info = mne.io.read_info(rawfile)
-
-# Hame has downsampled to 4ms per sample
-info['sfreq'] = 1 / 0.004
+if subjects is None:
+    subjects = helpers.find_available_subjects('data/meg_behav', 
+                                               'data/meg_final_data')
+subjects = np.sort(np.array(subjects))
+S = subjects.size
+if S > 1:
+    fname = pd.datetime.now().strftime('clusres_5thdot_meg_%Y%m%d%H%M')
 
 # create events array
 events = np.c_[np.arange(480), np.full(480, 301, dtype=int), 
@@ -34,31 +33,13 @@ events = np.c_[np.arange(480), np.full(480, 301, dtype=int),
 
 # type of event
 event_id = {'dot_onset': 0}
-               
-# load Hame's epoched data
-# conveniently, the order of the channels in the matlab data is the same as
-# the order given by the raw fif-file (see channel_names.mat which I extracted
-# from variable hdr in one of the files in meg_original_data)
-mat = loadmat('data/meg_final_data/%02d_sdat.mat' % sub)
-
-# construct full data array corresponding to the channels defined in info
-data = np.zeros((480, 313, 301))
-# MEG data
-data[:, :306, :] = mat['smeg']
-# EOG + ECG
-data[:, 306:309, :] = mat['setc'][:, :3, :]
-# triggers (STI101) - Hame must have added this manually as I can't see where
-# it was added in her Matlab scripts, but the time-courses and values 
-# definitely suggest that it's STI101
-# NOT PRESENT FOR ALL SUBJECTS
-if mat['setc'].shape[1] == 4:
-    data[:, 310, :] = mat['setc'][:, 3, :]
 
 # when does an epoch start? - 0.3 s before onset of the first dot
 tmin = -0.3
 
-# create MNE epochs
-epochs = mne.EpochsArray(data, info, events, tmin, event_id, proj=False)
+# channel connectivity from FieldTrip for cluster analysis
+connectivity, co_channels = mne.channels.read_ch_connectivity(
+    '/home/bitzer/proni/BeeMEG/MEG/neuromag306mag_neighb.mat')
 
 
 #%% build parametric regressor quantifying how much the 5th dot supports the 
@@ -68,82 +49,137 @@ trial_info = helpers.get_5th_dot_infos(dotpos)
 
 names = ['intercept', 'support_correct']
 design_matrix = np.c_[np.ones(480), trial_info['support_correct']]
-
 #design_matrix[:, 1] = design_matrix[np.random.randint(480, size=480), 1]
 
-lm = mne.stats.regression.linear_regression(epochs, design_matrix, names)
+
+#%% run subject specific analysis
+max_cluster_p = pd.Series([], name='p of largest cluster', 
+                          index=pd.Index([], name='subject'))
+clus_res = {}
+cluster_result = collections.namedtuple('cluster_result', 
+                                        ['T_obs', 'clusters', 'pval', 'H0'])
+for sub in subjects:
+    print('\nSubject %02d' % sub)
+    print('----------')
+    
+    try:
+        # raw file of subject, block 1
+        rawfile = '/home/bitzer/proni/BeeMEG/MEG/Raw/bm%02da/bm%02da1.fif' % (sub, sub)
+        
+        # get MEG-info from raw file (for channel layout and names)
+        info = mne.io.read_info(rawfile)
+    except FileNotFoundError:
+        # just use last working info - for now it shouldn't matter
+        pass
+
+    # Hame has downsampled to 4ms per sample
+    info['sfreq'] = 1 / 0.004
+
+    # load Hame's epoched data
+    # conveniently, the order of the channels in the matlab data is the same as
+    # the order given by the raw fif-file (see channel_names.mat which I extracted
+    # from variable hdr in one of the files in meg_original_data)
+    mat = loadmat('data/meg_final_data/%02d_sdat.mat' % sub)
+
+    # construct full data array corresponding to the channels defined in info
+    data = np.zeros((480, 313, 301))
+    # MEG data
+    data[:, :306, :] = mat['smeg']
+    # EOG + ECG
+    data[:, 306:309, :] = mat['setc'][:, :3, :]
+    # triggers (STI101) - Hame must have added this manually as I can't see where
+    # it was added in her Matlab scripts, but the time-courses and values 
+    # definitely suggest that it's STI101
+    # NOT PRESENT FOR ALL SUBJECTS
+    if mat['setc'].shape[1] == 4:
+        data[:, 310, :] = mat['setc'][:, 3, :]
+
+    # create MNE epochs
+    epochs = mne.EpochsArray(data, info, events, tmin, event_id, proj=False)
+    
+    # do extra analyses for single subject
+    if S == 1:
+        lm = mne.stats.regression.linear_regression(epochs, design_matrix, names)
+
+        #%% plot topomap of result
+        support_correct = lm['support_correct']
+        
+        ch_type = 'mag'
+        times = np.linspace(0, .7, 15)
+        times = 'peaks'
+        
+        # plot p-values
+        fig = support_correct.mlog10_p_val.plot_topomap(ch_type=ch_type, size=1.5, 
+            unit='-log10_p', vmin=0, vmax=4.0, scale=1, times=times);
+        
+        # extract times from titles
+        times = [float(ax.get_title()[:-3]) / 1000 for ax in fig.axes[:-1]]
+        
+        # plot t-values
+        support_correct.t_val.plot_topomap(ch_type=ch_type, size=1.5, unit='t', 
+            vmin=-4.0, vmax=4.0, scale=1, times=times);
+        
+                                           
+        #%% check linear regression manually
+        evo = support_correct.mlog10_p_val
+        chname, timeind = evo.get_peak('mag', time_as_index=True)
+        chind = np.flatnonzero(np.array(evo.ch_names) == chname)[0]
+        
+        df = pd.DataFrame(np.c_[data[:, chind, timeind], design_matrix[:, 1]], 
+                          columns=['data', 'support_correct'])
+        
+        res = smf.ols('data ~ support_correct', data=df).fit()
+        
+        print(res.summary())
+        
+        beta, stderr, t_val, p_val, mlog10_p_val = mne.stats.regression._fit_lm(
+            data, design_matrix, names)
+        
+        print('\n')
+        print('t-val statsmodels: {}\n'
+              't-val linregress:  {}\n'
+              't-val mne:         {}\n'.format(res.tvalues['support_correct'],
+              helpers.linregress_t(data[:, chind, timeind][:, None], design_matrix[:, 1])[0], 
+              support_correct.t_val.data[chind, timeind]))
+        
+        
+        #%% permutation test of identified peak
+        rind = np.random.randint(480, size=(480, 10000))
+        perm_t_vals = helpers.linregress_t(data[:, chind, timeind][rind], 
+                                           design_matrix[:, 1])
+        
+        ax = sns.distplot(perm_t_vals)
+        ax.plot(support_correct.t_val.data[chind, timeind], 0, 'r*')
+        
+        perm_p_val = np.mean(np.abs(perm_t_vals) >= 
+                             np.abs(support_correct.t_val.data[chind, timeind]))
+        print('permuted p-value:   {}\n'
+              'regression p-value: {}'.format(perm_p_val, 
+              support_correct.p_val.data[chind, timeind]))
 
 
-#%% plot topomap of result
-support_correct = lm['support_correct']
-
-ch_type = 'mag'
-times = np.linspace(0, .7, 15)
-times = 'peaks'
-
-# plot p-values
-fig = support_correct.mlog10_p_val.plot_topomap(ch_type=ch_type, size=1.5, 
-    unit='-log10_p', vmin=0, vmax=4.0, scale=1, times=times);
-
-# extract times from titles
-times = [float(ax.get_title()[:-3]) / 1000 for ax in fig.axes[:-1]]
-
-# plot t-values
-support_correct.t_val.plot_topomap(ch_type=ch_type, size=1.5, unit='t', 
-    vmin=-4.0, vmax=4.0, scale=1, times=times);
-
-                                   
-#%% check linear regression manually
-evo = support_correct.mlog10_p_val
-chname, timeind = evo.get_peak('mag', time_as_index=True)
-chind = np.flatnonzero(np.array(evo.ch_names) == chname)[0]
-
-df = pd.DataFrame(np.c_[data[:, chind, timeind], design_matrix[:, 1]], 
-                  columns=['data', 'support_correct'])
-
-res = smf.ols('data ~ support_correct', data=df).fit()
-
-print(res.summary())
-
-beta, stderr, t_val, p_val, mlog10_p_val = mne.stats.regression._fit_lm(
-    data, design_matrix, names)
-
-print('\n')
-print('t-val statsmodels: {}\n'
-      't-val linregress:  {}\n'
-      't-val mne:         {}\n'.format(res.tvalues['support_correct'],
-      helpers.linregress_t(data[:, chind, timeind][:, None], design_matrix[:, 1])[0], 
-      support_correct.t_val.data[chind, timeind]))
+    #%% permutation cluster analysis
+    epochs_mag = epochs.pick_types(meg='mag')
+    
+    # channels must have the same order
+    assert(np.all(np.array(epochs_mag.info['ch_names']) == co_channels))
+    
+    T_obs, clusters, pval, H0 = mne.stats.spatio_temporal_cluster_1samp_test(
+        epochs_mag.get_data().transpose(0, 2, 1), connectivity=connectivity, 
+        stat_fun=lambda data: helpers.linregress_t(data, design_matrix[:, 1]), 
+        tail=0, n_permutations=1024, threshold=3, max_step=1)
+    
+    max_cluster_p.loc[sub] = pval.min()
+    clus_res[sub] = cluster_result(T_obs, clusters, pval, H0)
+    
+    if S == 1:
+        ax = sns.distplot(H0, kde=False)
+        ax.plot([np.sum(T_obs[clus]) for clus in clusters], np.zeros(len(clusters)), '*r')
+    else:
+        np.savez_compressed('data/inf_results/' + fname, 
+                            max_cluster_p=max_cluster_p, clus_res=clus_res)
 
 
-#%% permutation test of identified peak
-rind = np.random.randint(480, size=(480, 10000))
-perm_t_vals = helpers.linregress_t(data[:, chind, timeind][rind], 
-                                   design_matrix[:, 1])
-
-ax = sns.distplot(perm_t_vals)
-ax.plot(support_correct.t_val.data[chind, timeind], 0, 'r*')
-
-perm_p_val = np.mean(np.abs(perm_t_vals) >= 
-                     np.abs(support_correct.t_val.data[chind, timeind]))
-print('permuted p-value:   {}\n'
-      'regression p-value: {}'.format(perm_p_val, 
-      support_correct.p_val.data[chind, timeind]))
-
-
-#%% permutation cluster analysis
-connectivity, co_channels = mne.channels.read_ch_connectivity(
-    '/home/bitzer/proni/BeeMEG/MEG/neuromag306mag_neighb.mat')
-
-epochs_mag = epochs.pick_types(meg='mag')
-
-# channels must have the same order
-assert(np.all(np.array(epochs_mag.info['ch_names']) == co_channels))
-
-T_obs, clusters, pval, H0 = mne.stats.spatio_temporal_cluster_1samp_test(
-    epochs_mag.get_data().transpose(0, 2, 1), connectivity=connectivity, 
-    stat_fun=lambda data: helpers.linregress_t(data, design_matrix[:, 1]), 
-    tail=0, n_permutations=1024, threshold=3, max_step=5)
-
-ax = sns.distplot(H0, kde=False)
-ax.plot([np.sum(T_obs[clus]) for clus in clusters], np.zeros(len(clusters)), '*r')
+#%% show distribution of p-values (should be uniform in [0,1] under H0)
+if S > 1:
+    sns.distplot(max_cluster_p, kde=False, rug=True)
