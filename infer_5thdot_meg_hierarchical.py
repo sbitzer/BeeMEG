d54f90afd575ad67a184b45fca3532429e05fa17
Sebastian Bitzer <sebastian.bitzer@tu-dresden.de>
2017-02-07 11:47:12 +0100

    new results with summary statistic approach (with and without bugfix)
    I rewrote the inference script to allow for different inference methods and
    implemented SPM's summary statistic approach. This runs much faster than ADVI. I
    also now exclude trials in which the RT was very soon (100ms) after the onset of
    the considered dot under the assumption that the MEG signal in these trials does
    not contribute to the dot-specific effects we are after. The output is now not
    only the negative log10 p-values, but also the underlying means and standard
    deviations.
    
    General Summary Statistics Results
    ----------------------------------
    I only looked at the p-values so far, but these are promising and reach values
    around 10^-8 for logpost_left. It is weird that in comparison to the ADVI
    results the effects for surprise and logpost_left appear reversed, i.e., in the
    summary statistic approach there is almost no effect of surprise (~10^-3), but a
    reasonable effect of logpost_left. Perhaps this is related to the interaction
    between the scaling of the regressors and the priors in ADVI and I should check
    that.
    
    I also observe strong effects of intercept and trial time with p-values smaller
    than 10^-8 and moderate effects of dot_y and entropy (~10^-6).
    
    The location of the effects seems to strongly overlap between logpost_left,
    entropy and trial_time with perhaps small offsets in timing around 650ms. I
    should investigate this.
    
    dot_y appears to have two peaks, one at 600 ms and one at 700ms. Is this due to
    the next dot being shown? But why is then no effect at 500 ms? Also, the
    y-positions of the dots should be uncorrelated. So there should be no effect of
    the ith dot at the (i+1)th time point. Perhaps this has something to do with the
    other regressors in the sense that the dot_y effect only is apparent when the
    effects of the other regressors are accounted for.
    
    Effect of removed trials
    ------------------------
    The notebook shows results for an old run in which the bug was still in effect,
    i.e., only for subject 2 trials were removed, because of nans in the surprise
    regressor. For these results I also kept trials with fast RTs. The results with
    the new code after the bugfix are very similar, though.
    
    Because of fewer number of trials you would expect p-values to increase, but
    because you supposedly remove noise from the data you would also expect p-values
    to decrease. The results are equally mixed. P-values changed slightly, but not
    much. For dot_y, surprise and trial_time the minimum p-values increased while
    they further decreased for the remaining regressors, e.g. logpost_left from
    10^-8.57 to 10^-8.73. Except for logpost_left and surprise the mean p-values
    across all times and channels tended to decrease.

--- infer_5thdot_meg_hierarchical.py
+++ infer_5thdot_meg_hierarchical.py
@@ -11,8 +11,22 @@ import helpers
 import pandas as pd
 import subject_DM
 import pymc3 as pm
-from scipy.stats import norm
+from scipy.stats import norm, ttest_1samp
 import os
+import statsmodels.api as sm
+
+
+#%% 
+# one of 'advi', 'nuts', 'ss'
+method = 'ss'
+
+fname = 'meg_hierarchical_' + method + '.h5'
+
+doti = 5
+
+# implements the assumption that in a trial with RT=(dot onset time + rt_thresh)
+# there cannot be an effect of the corresponding dot in the MEG signal
+rt_thresh = 0.1
 
 
 #%% load all data
@@ -23,59 +37,101 @@ S = subjects.size
 times = epochs_all.index.levels[2]
 
 
-#%% load trial-level design matrices for all subjects (5th-dot based)
-DM = subject_DM.get_ithdot_DM()
+#%% load trial-level design matrices for all subjects (ith-dot based)
+DM = subject_DM.get_ithdot_DM(doti=doti, r_names=['dot_y', 'surprise', 
+        'logpost_left', 'entropy', 'trial_time', 'intercept', 'RT'])
 DM = DM.loc(axis=0)[subjects, :]
+
+# remove trials which had too few simulations to estimate surprise
+good_trials = np.logical_not(np.isnan(DM['surprise'])).values
+                         
+# remove trials which had RTs below a threshold (and therefore most likely 
+# cannot have an effect of the shown dot)
+good_trials = np.logical_and(good_trials, 
+                             DM['RT'] >= helpers.dotdt*(doti-1) + rt_thresh)
+# remove RT from design matrix
+del DM['RT']
+
+# final number of regressors
 R = DM.shape[1]
 
-# remove trials which have nan
-nonan_idx = np.logical_not(np.isnan(DM['surprise'])).values
-DM = DM.loc[nonan_idx]
+# select only the good trials
+DM = DM.loc[good_trials]
 
 sub_map = {x: np.flatnonzero(subjects==x)[0] for x in subjects}
 subject_idx = DM.index.get_level_values('subject').map(lambda x: sub_map[x])
 
 
-#%% 
-# 2nd-level posterior probabilities
-def post_prob(mean, sd):
-    if mean > 0:
-        return norm.cdf(0, mean, sd)
-    else:
-        return 1 - norm.cdf(0, mean, sd)
+#%% prepare output and helpers
+second_level = pd.DataFrame([], 
+        index=pd.MultiIndex.from_product([epochs_all.columns, 
+              epochs_all.index.levels[2]], names=['channel', 'time']), 
+        columns=pd.MultiIndex.from_product([['mean', 'std', 'mlog10p'], 
+              DM.columns], names=['measure', 'regressor']), dtype=np.float64)
+second_level.sort_index(axis=1, inplace=True)
+
+if method == 'advi':
+    # 2nd-level posterior probabilities
+    def post_prob(mean, sd):
+        if mean > 0:
+            return norm.cdf(0, mean, sd)
+        else:
+            return 1 - norm.cdf(0, mean, sd)
 
 
-#%% define hierarchical GLM in pyMC3
-time = 500
-channel = 'MEG2041'
-pvals = pd.DataFrame([], index=pd.MultiIndex.from_product([epochs_all.columns, 
-                     epochs_all.index.levels[2]], names=['channel', 'time']), 
-                     columns=DM.columns, dtype=np.float64)
-
+#%% infer
 for time in times:
+    print('time = %d' % time)
     for channel in epochs_all.columns:
         data = epochs_all.loc[(slice(None), slice(None), time), channel]
-        data = data.loc[nonan_idx]
-
-        with pm.Model() as model:
-            beta2 = pm.Normal('beta2', sd=100**2, shape=R)
-            sigma2 = pm.HalfCauchy('sigma2', 5, shape=R, testval=.1)
+        data = data.loc[good_trials.values]
             
-            beta1 = pm.Normal('beta1', mu=beta2, sd=sigma2, shape=(S, R))
+        if method == 'ss':
+            params = np.zeros((S, R))
+            for s, sub in enumerate(subjects):
+                res = sm.OLS(data.loc[sub].values, DM.loc[sub].values, 
+                             hasconst=True).fit()
+                params[s, :] = res.params
+               
+            second_level.loc[(channel, time), ('mean', slice(None))] = (
+                    params.mean(axis=0))
+            second_level.loc[(channel, time), ('std', slice(None))] = (
+                    params.std(axis=0))
+            _, pvals = ttest_1samp(params, 0, axis=0)
+            second_level.loc[(channel, time), ('mlog10p', slice(None))] = (
+                    -np.log10(pvals))
+        else:
+            with pm.Model() as model:
+                beta2 = pm.Normal('beta2', sd=100**2, shape=R)
+                sigma2 = pm.HalfCauchy('sigma2', 5, shape=R, testval=.1)
+                
+                beta1 = pm.Normal('beta1', mu=beta2, sd=sigma2, shape=(S, R))
+                
+                eps = pm.HalfCauchy('eps', 5)
+                
+                pred = (beta1[subject_idx, :] * DM.values).sum(axis=1)
+                
+                likelihood = pm.Normal(channel, mu=pred, sd=eps, observed=data.values)
+                
+                means, sds, elbos = pm.variational.advi(n=40000)
             
-            eps = pm.HalfCauchy('eps', 5)
-            
-            pred = (beta1[subject_idx, :] * DM.values).sum(axis=1)
-            
-            likelihood = pm.Normal(channel, mu=pred, sd=eps, observed=data.values)
-            
-            means, sds, elbos = pm.variational.advi(n=40000)
-        
-        # compute "p-values"
-        pvals.loc[(channel, time), :] = np.array([post_prob(m, s) for m, s in 
-                 zip(means['beta2'], sds['beta2'])])
-
+            # compute "p-values"
+            pvals.loc[(channel, time), :] = np.array([post_prob(m, s) for m, s in 
+                     zip(means['beta2'], sds['beta2'])])
+    
+            try:
+                second_level.to_hdf(os.path.join(helpers.resultsdir, fname), 
+                                    'second_level', mode='w')
+            except:
+                pass
+    
+    # for summary statistic only save every time point
+    if method == 'ss':
         try:
-            pvals.to_hdf(os.path.join(helpers.resultsdir, 'meg_hierarchical_advi.h5'), 'pvals', mode='w')
+            second_level.to_hdf(os.path.join(helpers.resultsdir, fname), 
+                                'second_level', mode='w')
         except:
             pass
+    else:
+        # print new line for pyMC3-methods
+        print('')
