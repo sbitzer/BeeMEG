major bugfix + normalisation + ADVI overhaul + save options
Bugfix
------
regressors in design matrix were not aligned with names in output, consequently
my interpretations and descriptions from last commit should be remapped (the
order of the regressors in the output followed that of r_names variable now
saved in the results such that the last topomap in the notebook always showed
the intercept and not the trial_time as the figure label suggested); see below
for re-interpretation of results

Normalisation
-------------
I now normalise the regressors and data across all subjects and time points to
equalise potential contributions and minimise interactions with the prior in the
Bayesian model. I remember wondering about a strange change in results for one
of the regressors with the summary statistics approach where I did not expect a
change, but this might have been because I remapped the regressors in the
results at the same time such that the change was actually in the intercept, as
one would expect.

ADVI overhaul
-------------
Made ADVI work now with the new results structure and save posterior mean and
std for the second-level parameters. Also now save neg log10 p-values as
originally intended instead of raw p-values.

Saving options
--------------
Made most options, including choices for prior, explicit and now save them
together with the results in the HDF5-file.

Results
-------
I have looked at t-values (directly related to neg-log10-p-values but signed),
mean parameter values and their standard deviation across subjects.

logpost_left and entropy reach the largest mlog10p values (both ~8.7) while
dot_y reaches 6.5 and surprise and intercept are around 5.5. The chosen time
points in the topomaps do not necessarily show these differences clearly,
though.

NOTE that the topomaps can be deceiving. For example, it looks like the
intercept reaches very high t-values around 8 which is supported by the reported
number during mouse hover over the region, but the hig

infer_5thdot_meg_hierarchical.py
@@ -16,21 +16,45 @@ import os
 import statsmodels.api as sm
 
 
-#%% 
+#%% options
 # one of 'advi', 'nuts', 'ss'
 method = 'ss'
 
-fname = 'meg_hierarchical_' + method + '.h5'
+# where to store
+file = os.path.join(helpers.resultsdir, 'meg_hierarchical_' + method + '.h5')
 
+# which dot to investigate?
 doti = 5
 
-# implements the assumption that in a trial with RT=(dot onset time + rt_thresh)
+# implements the assumption that in a trial with RT<(dot onset time + rt_thresh)
 # there cannot be an effect of the corresponding dot in the MEG signal
 rt_thresh = 0.1
 
+# names of regressors that should enter the GLM
+r_names = ['dot_y', 'surprise', 'logpost_left', 'entropy', 'trial_time', 
+           'intercept']
+
+# number of ADVI iterations
+advi_n = 30000
+
+# priors of Bayesian hierarchical model
+prior = pd.Series({'beta2_sd': 100, 
+                   'sigma2_beta': 5, 
+                   'eps_beta': 5})
+
+# create HDF5-file and save chosen options
+with pd.HDFStore(file, mode='w', complevel=7, complib='zlib') as store:
+    store['doti'] = pd.Series(doti)
+    store['rt_thresh'] = pd.Series(rt_thresh)
+    if method in ['advi', 'nuts']:
+        store['prior'] = prior
+    if method == 'advi':
+        store['advi_n'] = pd.Series(advi_n)
+         
 
 #%% load all data
-epochs_all = helpers.load_meg_epochs(hfreq=10, window=[0.4, 0.7])
+window = doti * helpers.dotdt + np.array([-0.1, 0.2])
+epochs_all = helpers.load_meg_epochs(hfreq=10, window=window)
 subjects = epochs_all.index.levels[0]
 S = subjects.size
 
@@ -38,9 +62,16 @@ times = epochs_all.index.levels[2]
 
 
 #%% load trial-level design matrices for all subjects (ith-dot based)
-DM = subject_DM.get_ithdot_DM(doti=doti, r_names=['dot_y', 'surprise', 
-        'logpost_left', 'entropy', 'trial_time', 'intercept', 'RT'])
+if 'RT' in r_names:
+    DM = subject_DM.get_ithdot_DM(doti, r_names)
+else:
+    DM = subject_DM.get_ithdot_DM(doti, r_names+['RT'])
+    
 DM = DM.loc(axis=0)[subjects, :]
+
+# ensure that order of regressors in design matrix and results DataFrame 
+# (second_level) is the same
+DM.sort_index(axis=1, inplace=True)
 
 # remove trials which had too few simulations to estimate surprise
 good_trials = np.logical_not(np.isnan(DM['surprise'])).values
@@ -50,7 +81,8 @@ good_trials = np.logical_not(np.isnan(DM['surprise'])).values
 good_trials = np.logical_and(good_trials, 
                              DM['RT'] >= helpers.dotdt*(doti-1) + rt_thresh)
 # remove RT from design matrix
-del DM['RT']
+if 'RT' not in r_names:
+    del DM['RT']
 
 # final number of regressors
 R = DM.shape[1]
@@ -62,6 +94,16 @@ sub_map = {x: np.flatnonzero(subjects==x)[0] for x in subjects}
 subject_idx = DM.index.get_level_values('subject').map(lambda x: sub_map[x])
 
 
+#%% normalise data and regressors to simplify definition of priors
+DM = (DM - DM.mean()) / DM.std()
+# intercept will be nan, because it has no variance
+DM['intercept'] = 1
+
+# this will include bad trials, too, but it shouldn't matter as this 
+# normalisation across subjects, trials and time points is anyway very rough
+epochs_all = (epochs_all - epochs_all.mean()) / epochs_all.std()
+
+
 #%% prepare output and helpers
 second_level = pd.DataFrame([], 
         index=pd.MultiIndex.from_product([epochs_all.columns, 
@@ -69,6 +111,9 @@ second_level = pd.DataFrame([],
         columns=pd.MultiIndex.from_product([['mean', 'std', 'mlog10p'], 
               DM.columns], names=['measure', 'regressor']), dtype=np.float64)
 second_level.sort_index(axis=1, inplace=True)
+
+assert np.all(second_level.columns.levels[1] == DM.columns), 'order of ' \
+    'regressors in design matrix should be equal to that of results DataFrame'
 
 if method == 'advi':
     # 2nd-level posterior probabilities
@@ -102,34 +147,46 @@ for time in times:
                     -np.log10(pvals))
         else:
             with pm.Model() as model:
-                beta2 = pm.Normal('beta2', sd=100**2, shape=R)
-                sigma2 = pm.HalfCauchy('sigma2', 5, shape=R, testval=.1)
+                # second-level parameter values
+                beta2 = pm.Normal('beta2', sd=prior.beta2_sd**2, shape=R)
                 
+                # between-subject std
+                sigma2 = pm.HalfCauchy('sigma2', prior.sigma2_beta, shape=R)
+                
+                # subject-specific parameter values
                 beta1 = pm.Normal('beta1', mu=beta2, sd=sigma2, shape=(S, R))
                 
-                eps = pm.HalfCauchy('eps', 5)
+                # within-subject std
+                eps = pm.HalfCauchy('eps', prior.eps_beta)
                 
                 pred = (beta1[subject_idx, :] * DM.values).sum(axis=1)
                 
                 likelihood = pm.Normal(channel, mu=pred, sd=eps, observed=data.values)
                 
-                means, sds, elbos = pm.variational.advi(n=40000)
+                means, sds, elbos = pm.variational.advi(n=advi_n)
             
-            # compute "p-values"
-            pvals.loc[(channel, time), :] = np.array([post_prob(m, s) for m, s in 
-                     zip(means['beta2'], sds['beta2'])])
+            # compute "posterior p-values"
+            mlog10p = -np.log10(np.array([post_prob(m, s) for m, s in 
+                                          zip(means['beta2'], sds['beta2'])]))
+    
+            second_level.loc[(channel, time), ('mean', slice(None))] = (
+                    means['beta2'])
+            second_level.loc[(channel, time), ('std', slice(None))] = (
+                    sds['beta2'])
+            second_level.loc[(channel, time), ('mlog10p', slice(None))] = (
+                    mlog10p)
     
             try:
-                second_level.to_hdf(os.path.join(helpers.resultsdir, fname), 
-                                    'second_level', mode='w')
+                with pd.HDFStore(file, mode='r+', complevel=7, complib='zlib') as store:
+                    store['second_level'] = second_level
             except:
                 pass
     
-    # for summary statistic only save every time point
+    # for summary statistic save only every time point
     if method == 'ss':
         try:
-            second_level.to_hdf(os.path.join(helpers.resultsdir, fname), 
-                                'second_level', mode='w')
+            with pd.HDFStore(file, mode='r+', complevel=7, complib='zlib') as store:
+                store['second_level'] = second_level
         except:
             pass
     else: