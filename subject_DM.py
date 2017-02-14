bf5db9632bf7e33328559946c3012fb0d5330b12
Sebastian Bitzer <sebastian.bitzer@tu-dresden.de>
2017-02-14 17:41:25 +0100

    look into correlations between regressors across dots
    Made a new function which can return regressors of several dots and
    automatically names them according to their associated dot. This function makes
    previous get_ithdot_DM and get_ithdot_DM_single_sub obsolete, because it can
    also return single subject, single dot design matrices. I kept these other
    functions for now just to be safe in case I still use them somewhere. I verified
    that the new function returns the same regressors as the old functions.
    
    I have now checked the correlations of dot regressors across dots:
    
    logpost_left
    ............
    is strongly auto-correlated, i.e., logpost_left of successive dots usually
    correlate with correlation coefficients above 0.6. This decreases with
    increasing separation of the dots in time, falling below 0.3 for the correlation
    between the 1st and 5th dots. Interestingly, the correlation between 1st and 2nd
    dot is larger than the correlation between 4th and 5th. This is probably simply
    because logpost_left starts out at a common value.
    
    Anyway, these correlations suggest that it wouldn't be a good idea to include
    logpost_left across several dots into a common GLM.
    
    m_evidence_left
    ...............
    As one would expect, there is almost no correlation between the momentary
    evidences of different dots. As momentary_evidence is, on the other hand,
    strongly correlated (~0.87) with logpost_left I think that it could be worth
    including it instead of logpost_left in the GLM, but for all dots. As, there is
    a perfect anti-correlation between m_evidence_left and dot_x, I can equivalently
    include dot_x into the GLM.
    
    I should then think of how I can investigate the accumulated evidence on top of
    the momentary evidence. After all, what is the advantage of having the model
    then?

--- subject_DM.py
+++ subject_DM.py
@@ -6,12 +6,62 @@ Created on Tue Jan 31 15:43:48 2017
 @author: bitzer
 """
 
+import numpy as np
 import pandas as pd
 import regressors
 import seaborn as sns
 
 
 #%% 
+def get_trial_DM(dots=5, subjects=None, r_names=['dot_y', 'surprise', 
+                 'logpost_left', 'entropy', 'trial_time', 'intercept']):
+    if subjects is None:
+        subjects = regressors.subjecti
+    else:
+        subjects = np.atleast_1d(subjects)
+        
+    dots = np.atleast_1d(dots)
+    
+    if r_names == 'all':
+        r_names = np.concatenate((regressors.trial.columns, 
+                                  regressors.trial_dot.columns,
+                                  regressors.subject_trial.columns,
+                                  regressors.subject_trial_dot.columns))
+
+    # selected regressor names sorted by dependence
+    tr_rn = np.intersect1d(r_names, regressors.trial.columns)
+    tr_d_rn = np.intersect1d(r_names, regressors.trial_dot.columns)
+    s_tr_rn = np.intersect1d(r_names, regressors.subject_trial.columns)
+    s_tr_d_rn = np.intersect1d(r_names, regressors.subject_trial_dot.columns)
+
+    DM = []
+    for sub in subjects:
+        dm = pd.concat([regressors.trial[tr_rn], 
+                        regressors.subject_trial.loc[sub, s_tr_rn]], 
+                       axis=1)
+        for dot in dots:
+            tr_d = regressors.trial_dot.xs(dot, level='dot')[tr_d_rn]
+            tr_d.columns = tr_d.columns.map(lambda x: x+'_%d' % (dot, ))
+            
+            s_tr_d = regressors.subject_trial_dot.loc[sub, s_tr_d_rn].xs(dot, level='dot')
+            s_tr_d.columns = s_tr_d.columns.map(lambda x: x+'_%d' % (dot, ))
+            
+            dm = pd.concat([dm, tr_d, s_tr_d], axis=1)
+            
+        dm['subject'] = sub
+        dm.set_index('subject', append=True, inplace=True)
+        dm = dm.reorder_levels(['subject', 'trial'])
+        
+        DM.append(dm)
+        
+    DM = pd.concat(DM)
+    
+    if 'intercept' in r_names:
+        DM['intercept'] = 1
+    
+    return DM
+            
+
 def get_ithdot_DM_single_sub(sub, doti=5, intercept=True):
     # subject unspecific
     DM = pd.concat([regressors.trial_dot.xs(doti, level='dot'), 
