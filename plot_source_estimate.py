8e7225606122ec03fa86a4fde23c089b3f365ac4
Sebastian Bitzer <sebastian.bitzer@tu-dresden.de>
2017-03-20 17:09:42 +0100

    argparse for argument 'file pattern' to select between source reconstructions

--- plot_source_estimate.py
+++ plot_source_estimate.py
@@ -12,28 +12,32 @@ import pandas as pd
 import numpy as np
 import sys
 import glob
-
+import argparse
 from mayavi import mlab
+
+parser = argparse.ArgumentParser(description='Show reconstructed sources using pySurfer.')
+parser.add_argument('r_name', nargs='?', default='dot_x',
+                    help='the name of the regressor for which sources should be shown')
+parser.add_argument('f_pattern', nargs='?', default='',
+                    help='file pattern identifying the reconstructed sources (default: "")')
+
+args = parser.parse_args()
 
 subjects_dir = 'mne_subjects'
 subject = 'fsaverage'
 bem_dir = os.path.join(subjects_dir, subject, 'bem')
 
-if len(sys.argv) < 2:
-    r_name = 'dot_x'
-else:
-    r_name = sys.argv[1]
-
 # find files
-files = glob.glob(os.path.join(bem_dir, '*_source_' + r_name + '-lh.stc'))
+files = glob.glob(os.path.join(  bem_dir, '*' + args.f_pattern + '*' 
+                               + '_source_' + args.r_name + '-lh.stc'))
 
 if len(files) > 0:
     print "showing file: " + files[0]
     stc = mne.read_source_estimate(files[0])
 
-    brain = stc.plot(subject='fsaverage', surface='white', time_viewer=True, hemi='both')
+    brain = stc.plot(subject='fsaverage', surface='inflated', time_viewer=True, hemi='both')
     brain.show_view('lateral')
     
-    mlab.show()
+    mlab.show(stop=True)
 else:
     raise ValueError('No appropriate source file found!')
