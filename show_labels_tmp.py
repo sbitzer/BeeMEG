51aa2439598c84d55c2ba6e32677e0f976a6bda9
Sebastian Bitzer <sebastian.bitzer@tu-dresden.de>
2017-05-29 19:23:01 +0200

    plotting region time courses instead of brain
    Even with the aggregation into regions, there are still 12 (left) and 14 (right)
    regions activated for the dot_x regressor which makes results still unclear. To
    counter that I plot only maximally 3 time courses in one panel. This at least
    allows to identify individual time courses and I can start to interpret them.
    
    In the time courses for dot_x I recognise the activations at 120 and 170 ms, but
    the 120 ms peaks appear to be comparably weak. I also see the sustained
    activations from 300 ms onwards. Additionally, however, there are also effects
    in between 170 and 300 ms which I would like to interpret as the interesting
    computations that actually lead to the updating of the accumulated evidence
    supposedly reflected after 300 ms. I will need to check the sequence of peaks
    across regions in that time slot to make this more concrete.
    
    I also briefly checked the results for the accev regressor. These slightly
    differ for the left and right hemisphere (as for dot_x). However, both
    hemispheres have in common that anterior cingulate and medial prefrontal cortex
    as well as posterior cingulate cortex show sustained activation through almost
    the whole time period. In the right hemisphere this is also true for the dorsal
    stream visual cortex. The other regions show intermittent activation and I
    haven't identified a particular pattern in there, yet.
    
    What does an intermittent representation of accumulated evidence in an area
    mean? Given that accumulated evidence is correlated with dot_x what are the
    patterns that you should expect to see in accumulated evidence based on the
    patterns in dot_x and when would you not expect to see dot_x patterns in accev?
    In any case, if dot_x patterns are to be found in accev, then they should occur
    100 ms earlier assuming that accev mostly reflects the dot_x patterns of the
    previous dot.

--- show_labels_tmp.py
+++ show_labels_tmp.py
@@ -3,6 +3,7 @@ import pandas as pd
 from surfer import Brain
 from surfer import TimeViewer
 import numpy as np
+import matplotlib.pyplot as plt
 
 r_name = 'dot_x'
 measure = 'mu_p_large'
@@ -11,21 +12,65 @@ srcfile = basefile[:-3] + '_slabs_%s.h5' % r_name
 file = 'mne_subjects/fsaverage/bem/' + srcfile
 src_df = pd.read_hdf(file, 'second_level_src')
 
-brain = Brain('fsaverage', 'both', 'inflated', cortex='low_contrast',
-              subjects_dir=sv.subjects_dir)
+#brain = Brain('fsaverage', 'both', 'inflated', cortex='low_contrast',
+#              subjects_dir=sv.subjects_dir)
 
 threshold = sv.find_slabs_threshold(basefile, measure, quantile=0.95)
 
-labels = sv.show_labels_as_data(src_df, measure, brain, transparent=True,
-                                threshold=threshold, 
-                                region_aggfun=lambda a: np.max(a, axis=0))
 
-tv = TimeViewer(brain)
+
+#labels = sv.show_labels_as_data(src_df, measure, brain, transparent=True,
+#                                threshold=threshold, 
+#                                region_aggfun=lambda a: np.max(a, axis=0))
+#
+#tv = TimeViewer(brain)
+
+
+#%% show region time courses
+values = src_df[measure]
+
+# remove 'insignificant' data
+values = sv.null_inconsistent_measure(values, threshold)
+
+region_aggfun=lambda a: np.max(a, axis=0)
+
+regions = pd.DataFrame(
+        [], dtype=float, index=values.index.levels[1], columns=
+        pd.MultiIndex.from_product([['L', 'R'], sv.Glasser_sections.name], 
+                                   names = ['hemi', 'region']))
+for section in sv.Glasser_sections.index:
+    # find all areas in region
+    areas = sv.Glasser_areas[sv.Glasser_areas['main section'] == section]
+    
+    for hemi in ['l', 'r']:
+        # create full label names
+        labels = areas['area name'].map(
+                lambda s: '%s_%s_ROI-%sh' % (hemi.upper(), s, hemi))
+        
+        # set values of all areas to that of aggregated value
+        avals = values.loc[(labels, slice(None))]
+        avals = avals.reset_index(level='time').pivot(columns='time')
+        regions.loc[:, (hemi.upper(), sv.Glasser_sections.loc[section, 'name'])] = (
+                region_aggfun(avals).values)
+
+active_regions = regions.drop(regions.columns[regions.sum() == 0], axis=1)
+
+for hemi in ['L', 'R']:
+    active = active_regions.loc[:, hemi]
+    N = active.shape[1]
+    fig, axes = plt.subplots(int(np.ceil(N/3.0)), 1, sharex=True)
+    
+    for i, ax in enumerate(axes):
+        active.iloc[:, slice(i*3, (i+1)*3)].plot.line(ax=ax)
+        ax.set_ylim([threshold, 1.0])
+        
+    axes[0].set_title('hemisphere: ' + hemi)
+    
+plt.show()
 
 
 #%% 
 from itertools import groupby
-import matplotlib.pyplot as plt
 
 def count_consecutive_nonzeros(iterable):
     return np.array([sum(1 for _ in group) 
