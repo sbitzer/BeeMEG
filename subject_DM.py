bugfix + trialregs_dot=0 + normalising standard deviations
normalisation
-------------
I have decided to switch back to scaling regressors so that their standard
deviation equals 1. The main reason is comparability in the resulting
beta-estimates. In particular, I have confirmed that the beta of a regressor
halves when the scaling of the regressor doubles. I was worried that as a result
of the vector-based normalisation the unsigned regressors (e.g., abs_dot_x)
consistently have higher betas than the signed ones, because their standard
deviation is only around 0.5 while it is around 1 for the signed regressors. It
may be the case that the vector-based normalisation makes more sense with
respect to the underlying physical units, but when I want to compare the sizes
of effects across regressors, I think the normalisation with respect to
standard deviation makes more sense. At least then I have a common statistical
unit.

trialregs_dot
-------------
During these considerations I also concluded that it doesn't make sense at all
to set parts of the trial regressors to 0. Already for the last analysis, when
thinking about sensible normalisation of regressors, I thought that it's not the
greatest idea to include trial regressors in the dot-based sequential analysis,
because they cannot exhibit any sequential effects.

When normalising the design matrix such that the stds are 1, I noticed a strange
effect of trial_time: I expected that the std of trial regressors is below 1
after I set values for 2 of the three dots to 0, but trial_time had std greater
than 1. This was because trial_time defines a uniform distribution, but adding
probability mass to one of the edges of the distribution does apparently not
change the std much. I didn't expect that. In any case, it tipped the scales for
me towards thinking that setting values of trial regressors to 0 doesn't make
sense, because that introduced strong differences in the stds of the trial
regressors (response and entropy close 

subject_DM.py
@@ -102,26 +102,25 @@ def get_ithdot_DM(doti=5, r_names=['dot_y', 'surprise', 'logpost_left',
 
 #%% normalisation
 def normfun(regressor, msratio=2):
-    # subtract mean, if you can get into trouble with collinearity with the 
-    # intercept, otherwise leave it be
     mean = regressor.mean()
     std = regressor.std()
-    if abs(mean) > msratio * std:
-        # subtracting the mean ensures that the regressor is orthogonal to the
-        # intercept, any average effect of the regressor will move to the 
-        # intercept such that only the variance of the signal with the 
-        # regressor remains, but don't remove the intercept
-        if not (mean==1 and std==0):
-            regressor = regressor - mean
     
-    # scale regressor such that its length is equal to the sqrt of its number
-    # of elements, this unifies the length of all regressors in a design matrix
-    # to the length of the intercept regressor consisting of all 1s, the 
-    # procedure makes all positive or negative regressors have standard 
-    # deviation close to 0.5 while regressors with varying sign will tend to 
-    # have standard deviation close to 1
-    return regressor / np.sqrt((regressor ** 2).sum() / regressor.size)
-
+    # don't normalise the intercept
+    if mean==1 and std==0:
+        return regressor
+    else:
+        # subtract mean, if you can get into trouble with collinearity with the 
+        # intercept, otherwise leave it be
+        if abs(mean) > msratio * std:
+            # subtracting the mean ensures that the regressor is orthogonal to the
+            # intercept, any average effect of the regressor will move to the 
+            # intercept such that only the variance of the signal with the 
+            # regressor remains
+            regressor = regressor - mean
+        
+        # scale regressor so that it's standard deviation = 1
+        return regressor / std
+    
 
 def normalise_DM(DM, normalise, msratio=2):
     if normalise:
@@ -165,7 +164,7 @@ def cnumfun(DM):
     DM = DM[:, np.any(DM != 0, axis=0)]
     
     # ensure that regressor vectors have length 1
-    DM = DM / np.sqrt((DM ** 2).sum())
+    DM = DM / np.sqrt((DM ** 2).sum(axis=0))
     
     eig = np.linalg.eigvals(np.dot(DM.T, DM))
     