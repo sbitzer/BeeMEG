bb76b1a853c5c1c1a18b74e49dff3e1619dca889
Sebastian Bitzer <sebastian.bitzer@tu-dresden.de>
2017-09-07 18:11:02 +0200

    updated singledot source analysis for dots 3,4,5 with accev
    This singledot analysis is part of my investigations into accumulated evidence.
    For that purpose I have included the accev regressor for the 3rd dot only which
    consists of the sum of the first 2 dots. To avoid collinearities I therefore had
    to exclude the first 2 dots from the analysis. I included specifically accev_3,
    because that is the first accumulated evidence that is not directly related to
    dot_x, because it is a sum of two dot_xs. Also it's early enough so that it may
    not be related to the executed response. Note that the response regressor is
    still correlated with the dot_x regressors, but only with coefficients below
    0.4. The condition number of the design matrix also was around 10 when I
    included abs_dot_x and abs_dot_y, but it dropped to values around 5, when I
    excluded the two regressors. This is a bit strange as the correlations between
    response and dot_x were actually larger than the correlations with abs_dot_x and
    _y.
    
    I updated the analysis to include the new normalisation of the design matrix and
    the previously forgotten normalisation of the data.
    
    Results will be available in:
    	source_singledot_201709071126.h5

--- infer_singledot_source.py
+++ infer_singledot_source.py
@@ -12,30 +12,47 @@ import pandas as pd
 import subject_DM
 from scipy.stats import ttest_1samp
 import os
+import re
 import statsmodels.api as sm
 from warnings import warn
 
 
 #%% options
 # which dot to investigate?
-dots = np.arange(1, 6)
+dots = np.arange(3, 6)
 
 # implements the assumption that in a trial with RT<(dot onset time + rt_thresh)
 # there cannot be an effect of the last considered dot in the MEG signal
 rt_thresh = 0.1
 
 # names of regressors that should enter the GLM
-r_names = ['abs_dot_x', 'abs_dot_y', 'dot_x', 'dot_y', 'entropy', 'trial_time', 
-           'intercept', 'response']
+r_names = ['dot_x', 'dot_y', 'accev_3', 
+           'sum_dot_y_prev_3', 'entropy', 'trial_time', 'intercept', 
+           'response']
 R = len(r_names)
 
 # source data to use
-srcfile = 'source_epochs_allsubs_HCPMMP1_201706131725.h5'
+
+# label mode = mean
+srcfile = 'source_epochs_allsubs_HCPMMP1_201708232002.h5'
+
+# label mode = mean_flip
+#srcfile = 'source_epochs_allsubs_HCPMMP1_201706131725.h5'
+
+# label mode= None, lselection=[V1, V2], window=[0, 0.4]
+#srcfile = 'source_epochs_allsubs_HCPMMP1_201708151431.h5'
+
+# label mode = (abs) max
+#srcfile = 'source_epochs_allsubs_HCPMMP1_201706271717.h5'
 
 srcfile = os.path.join('mne_subjects', 'fsaverage', 'bem', srcfile)
 
 # How many permutations should be computed?
 nperm = 3
+
+# whether to normalise the source signal to have mean 0 and std 1 in each area
+# across trials, time points and subjects
+normsrc = True
 
 # where to store
 file = pd.datetime.now().strftime('source_singledot'+'_%Y%m%d%H%M'+'.h5')
@@ -45,7 +62,8 @@ file = os.path.join(helpers.resultsdir, file)
 
 # create HDF5-file and save chosen options
 with pd.HDFStore(file, mode='w', complevel=7, complib='blosc') as store:
-    store['scalar_params'] = pd.Series([rt_thresh], ['rt_thresh'])
+    store['scalar_params'] = pd.Series([rt_thresh, float(normsrc)], 
+         ['rt_thresh', 'normsrc'])
     store['dots'] = pd.Series(dots)
     store['srcfile'] = pd.Series(srcfile)
 
@@ -62,16 +80,27 @@ times = epochs.index.levels[2]
 
 #%% load trial-level design matrices for all subjects (ith-dot based)
 if 'RT' in r_names:
-    DM = subject_DM.get_trial_DM(dots, r_names=r_names)
+    DM = subject_DM.get_trial_DM(dots, subjects, r_names=r_names)
 else:
-    DM = subject_DM.get_trial_DM(dots, r_names=r_names+['RT'])
+    DM = subject_DM.get_trial_DM(dots, subjects, r_names=r_names+['RT'])
+
+# add selected single dot regressors
+single_r_names = []
+single_r_names_components = []
+for r_name in r_names:
+    match = re.match(r'(.*)_(\d)$', r_name)
+    if match:
+        single_r_names.append(r_name)
+        single_r_names_components.append(match.groups())
+
+DM = pd.concat([DM]+[subject_DM.get_trial_DM(int(dot), subjects, r_names=[r_name]) 
+                     for r_name, dot in single_r_names_components],
+               axis=1)
 
 # move_dist_1 is constant at 0, we don't need to fit that
 if 'move_dist' in r_names:
     del DM['move_dist_1']
     
-DM = DM.loc(axis=0)[subjects, :]
-
 # remove trials which had RTs below a threshold (and therefore most likely 
 # cannot have an effect of the last considered dot)
 good_trials = DM['RT'] >= (helpers.dotdt*(dots.max()-1) + rt_thresh)
@@ -84,17 +113,33 @@ DM = DM.loc[good_trials]
 
 # sort regressor names (columns) for indexing
 DM.sort_index(axis=1, inplace=True)
-R = DM.shape[1]
 
 
-#%% normalise data and regressors to simplify definition of priors
-DM = (DM - DM.mean()) / DM.std()
-# intercept will be nan, because it has no variance
-DM['intercept'] = 1
+#%% normalise design matrix to simplify definition of priors
+DM = subject_DM.normalise_DM(DM, 'subject')
+
+if DM.isnull().any().any():
+    raise ValueError("Design matrix has NaNs somewhere, please check!")
+
+
 
 # just get mean and std of data across all subjects, trials and times
 epochs_mean = pd.read_hdf(srcfile, 'epochs_mean')
 epochs_std = pd.read_hdf(srcfile, 'epochs_std')
+
+
+#%% check collinearity in design matrix using condition number
+# numbers from 10 indicate some problem with collinearity in design matrix
+# (see Rawlings1998, p. 371)
+DM_condition_numbers = subject_DM.compute_condition_number(DM, scope='subject')
+
+with pd.HDFStore(file, mode='r+', complevel=7, complib='blosc') as store:
+    store['DM_condition_numbers'] = DM_condition_numbers
+
+# warn, if there are more than 2 subjects with condition numbers above 10 at
+# any time point from 100 ms within the trial
+if (DM_condition_numbers > 10).sum() > 2:
+    warn("High condition numbers of design matrix detected!")
 
 
 #%% prepare output and helpers
@@ -121,7 +166,7 @@ second_level = pd.DataFrame([],
         index=pd.MultiIndex.from_product([np.arange(nperm+1), 
               srclabels, times], 
               names=['permnr', 'label', 'time']), 
-        columns=pd.MultiIndex.from_product([['mean', 'std', 'mlog10p'], 
+        columns=pd.MultiIndex.from_product([['mean', 'std', 'tval', 'mlog10p'], 
               DM.columns], names=['measure', 'regressor']), dtype=np.float64)
 second_level.sort_index(axis=1, inplace=True)
 
@@ -151,10 +196,15 @@ for perm in np.arange(nperm+1):
     with pd.HDFStore(srcfile, 'r') as store:
         for s, sub in enumerate(subjects):
             epochs = store.select('label_tc', 'subject=sub')
+            
+            # normalise each label across time points and subjects
+            if normsrc:
+                epochs = (epochs - epochs_mean) / epochs_std
+            
             epochs.loc[:] = epochs.values[permutation.flatten(), :]
             
             for t0 in times:
-                print('\rsubject = %2d, t0 = %3d' % (sub, t0), end='')
+                print('\rsubject = %2d, t0 = %3d' % (sub, t0), end='', flush=True)
         
                 data = epochs.loc[(sub, slice(None), t0)]
                 data = data.loc[good_trials.loc[sub].values]
@@ -186,7 +236,9 @@ for perm in np.arange(nperm+1):
                     params.mean().values)
             second_level.loc[(perm, label, t0), ('std', slice(None))] = (
                     params.std().values)
-            _, pvals = ttest_1samp(params.values, 0, axis=0)
+            tvals, pvals = ttest_1samp(params.values, 0, axis=0)
+            second_level.loc[(perm, label, t0), ('tval', slice(None))] = (
+                tvals)
             second_level.loc[(perm, label, t0), ('mlog10p', slice(None))] = (
                     -np.log10(pvals))
             
