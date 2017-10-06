70481b651968874e4702c2a7e8be2370a1f06299
Sebastian Bitzer <sebastian.bitzer@tu-dresden.de>
2017-10-06 12:53:39 +0200

    added choice-evidence-correlations figure

--- make_behaviour_figures.py
+++ make_behaviour_figures.py
@@ -7,6 +7,7 @@ Created on Mon Jul 10 18:21:58 2017
 """
 
 import regressors
+import subject_DM
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -41,3 +42,36 @@ fig.subplots_adjust(left=0.1, bottom=0.17, right=0.97, top=0.91, wspace=0.3)
 fig.savefig(os.path.join(figdir, 'pooledRTs.png'),
             dpi=300)
 
+
+#%% plot evidence-choice correlations
+dots = np.arange(1, 15)
+DM = subject_DM.get_trial_DM(dots, r_names=['dot_x', 'sum_dot_x', 'response'])
+
+def get_corrs(r_name):
+    dmcols = [name for name in DM.columns if ((name == 'response') or 
+                                              (name.startswith(r_name)))]
+    
+    correlations = DM.groupby(level='subject').apply(
+            lambda dm: dm[dmcols].corr().loc['response'])
+    
+    del correlations['response']
+    correlations.columns = pd.Index(dots, name='dot')
+    
+    return correlations
+
+correlations = pd.concat([get_corrs(name) for name in ['dot_x', 'sum_dot_x']],
+                         keys=['momentary', 'accumulated'], 
+                         names=['evidence', 'dot'], axis=1)
+correlations = correlations.stack().stack().reset_index()
+
+fig, ax = plt.subplots(1, figsize=(6, 4))
+
+sns.stripplot('dot', 0, 'evidence', data=correlations, jitter=True, 
+              dodge=True, ax=ax)
+ax.set_ylabel('correlation with choice');
+#ax.set_ylim([0, 1])
+
+fig.subplots_adjust(top=0.95, bottom=0.12, left=0.11, right=0.97)
+
+fig.savefig(os.path.join(figdir, 'choice_correlations.png'),
+            dpi=300)
