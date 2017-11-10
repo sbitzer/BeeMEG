3201b878ab19a5a1356a209d68c7354843de43a2
Sebastian Bitzer <sebastian.bitzer@tu-dresden.de>
2017-11-10 16:39:47 +0100

    compare GP vs simple average, summary statistics uncertainty
    To check whether the GP-based analysis gives reasonable results I computed an
    equivalent analysis by simply averaging the corresponding data at the given
    time points. The result is much noisier than the GP result and the GP noiseless
    prediction does indeed look like the underlying noise-removed, smooth time
    course. So the GP analysis appears very reasonable.
    
    Alternative to the std of the mean prediction I also tested the application of
    the summary statistics approach to the computation of the across-subject mean
    time course. This amounts to simply ignoring the stds of the subject-wise
    predictions and estimating uncertainty exclusively from the mean predictions of
    the subjects, i.e., you just compute the standard error of the average across
    subjects. The resulting standard error is an order of magnitude or so larger
    than the standard deviation of the mean prediction, but I'm undecided which one
    to trust more. The summary statistics approach does not really apply here as
    the number of trials varies across subjects and I cannot assume that the means
    have equal variance. Also, the distribution of the mean prediction appears to
    be a reasonable measure in the context of GP prediction. On the other hand,
    larger uncertainty estimates usually don't hurt and the mean prediction may
    conceptually be a questionable measure when you really do inference over a
    hidden mean across subjects, not prediction.

--- infer_timecourse_source_GP.py
+++ infer_timecourse_source_GP.py
@@ -28,6 +28,9 @@ file = 'source_epochs_allsubs_HCPMMP1_201708232002.h5'
 # area that should be investigated
 area = '4'
 
+# can be either 'gp' for sparse GP or 'mean' for simple average
+method = 'gp'
+
 timeslice = [0, 890]
 
 normsrc = True
@@ -72,9 +75,12 @@ choices = regressors.subject_trial.response.loc[subjects]
 store = pd.HDFStore(srcfile, 'r')
 
 Z = np.linspace(*timeslice, 15)
-xpred = np.linspace(*timeslice, 200)
+if method == 'gp':
+    xpred = np.linspace(*timeslice, 200)
+else:
+    xpred = np.arange(timeslice[0], timeslice[1] + 10, 10)
 
-gppred = pd.DataFrame([], 
+pred = pd.DataFrame([], 
         index=pd.MultiIndex.from_product(
                 [subjects, labels, xpred], 
                 names=['subject', 'label', 'time']),
@@ -103,13 +109,20 @@ for sub in subjects:
         if normsrc:
             data = (data - epochs_mean[label]) / epochs_std[label]
         
-        # fit the sparse GP
-        gpm = fitgp(data.index.get_level_values('time').values, data.values, Z)
-        
-        # get the noiseless prediction
-        gpmean, gpvar = gpm.predict_noiseless(xpred[:, None])
-        gppred.loc[(sub, label, slice(None)), 'mean'] = gpmean[:, 0]
-        gppred.loc[(sub, label, slice(None)), 'std'] = np.sqrt(gpvar[:, 0])
+        if method == 'gp':
+            # fit the sparse GP
+            gpm = fitgp(data.index.get_level_values('time').values, 
+                        data.values, Z)
+            
+            # get the noiseless prediction
+            gpmean, gpvar = gpm.predict_noiseless(xpred[:, None])
+            pred.loc[(sub, label, slice(None)), 'mean'] = gpmean[:, 0]
+            pred.loc[(sub, label, slice(None)), 'std'] = np.sqrt(gpvar[:, 0])
+        else:
+            pred.loc[(sub, label, slice(None)), 'mean'] = \
+                data.groupby('time').mean().values
+            pred.loc[(sub, label, slice(None)), 'std'] = \
+                data.groupby('time').std().values / np.sqrt(subjects.size)
         
     gc.collect()
         
@@ -120,12 +133,13 @@ colors = sns.cubehelix_palette(2, start=0.9, rot=0, light=.6, dark=.2)
 fig, axes = plt.subplots(1, 2, sharex=True, sharey=True, figsize=[7.5, 4.5])
 
 for ax, label in zip(axes, labels):
-    labelpred = gppred.xs(label, level='label').unstack('subject')
+    labelpred = pred.xs(label, level='label').unstack('subject')
     lsubs = ax.plot(xpred / 1000, labelpred['mean'], color=colors[0], lw=1)
     
     # compute mean prediction across subjects taking uncertainty into account
     meanpred = labelpred['mean'].mean(axis=1)
     stdpred = np.sqrt((labelpred['std'] ** 2).sum(axis=1) / subjects.size ** 2)
+#    stdpred = labelpred['mean'].std(axis=1) / np.sqrt(subjects.size)
     
     lmean, = ax.plot(xpred / 1000, meanpred, color=colors[1], lw=2)
     ax.fill_between(xpred / 1000, meanpred - 2 * stdpred, 
@@ -138,5 +152,6 @@ ax.legend([lsubs[0], lmean], ['participants', 'mean'])
 axes[0].set_ylabel('normalised mean currents in area %s' % area)
 fig.subplots_adjust(top=0.92, right=0.97, left=0.1, wspace=0.08)
 
-fname = os.path.join(figdir, 'general_contralateral_buildup_%s.png' % area)
+fname = os.path.join(figdir, 'general_contralateral_buildup_%s_%s.png' % (
+        area, method))
 fig.savefig(fname, dpi=300)
