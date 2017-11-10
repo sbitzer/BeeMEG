3080cd62013cbbd230c125527ef7f2498f121e2e
Sebastian Bitzer <sebastian.bitzer@tu-dresden.de>
2017-11-10 12:41:50 +0100

    investigating build-up in primary motor cortex from trial onset
    Given that the effects in primary motor cortex look very similar at 400 ms from
    dot onset and at the time of the response, my current hypothesis about the
    involvement of primary motor cortex in perceptual decision making is that it
    pre-activates movements proportional to the current evidence. The question
    remains why nobody reported this before. Perhaps they simply attributed any
    build-up in primary motor cortex to unspecific motor preparation. But then I
    also wonder why I don't actually see any slow build-up in the response aligned
    analysis, but only effects within 50 ms from the response. Also I wondered,
    when primary motor cortex activates proportional to (accumulated) evidence,
    shouldn't I see a general build-up of currents starting from first dot onset?
    Even if (accumulated) evidence is only represented from about 300 ms after dot
    onset, on average there should be a continuous representation of evidence from
    300 ms after first dot onset. Under the assumption that evidence for a left
    decision on average gradually builds up towards a left choice, the build-up
    should be seen in the contralateral (right) primary motor cortex.
    
    So in the present analysis I try to estimate the timecourse of the average
    motor currents prior to a contralateral choice starting from trial / first dot
    onset. I would expect that this starts to rise from 300 ms, because this is
    when I supposedly first see representations of evidence in primary motor
    cortex. I use sparse GPs to denoise the time courses from individual trials
    within subjects and then compute a weighted average across subjects.
    
    There is build-up, but it doesn't appear to start at 300 ms. In the left
    hemisphere the overall mean is pretty flat until a rise starts around 400 ms.
    In the right hemisphere the overall mean drops until about 200 ms after which
    it rises to a similar initial level as in the left hemisphere (reaching that
    level around 330 ms) and then rises further from about 400 ms, but less
    strongly than in the left hemisphere. Given that the largest dot-centred
    effects of evidence in primary motor cortex are around 400 ms, the start of the
    rise at 400 ms appears to be consistent with my considerations with the
    limitation that I wonder why the smaller effects between 300 and 400 ms are not
    represented here.
    
    A complication of this analysis is that I only observe 300 ms of the build-up
    and the end of this period is already the start of when I see average response
    effects (at 700 ms). This also means that this build-up is not necessarily a
    direct effect of the response. The response effects should come on top of this
    build-up. My prediction therefore is that from 700 ms there should be a visible
    increase in the slope of the build-up. I should re-run the analysis for the
    complete time window up to 890 ms to find out.
    
    One thing to keep in mind is that this analysis is very closely related to the
    correlation of the motoresplin regressor with the signal in primary motor
    currents (done in a previous version of make_correlation_figures.py).

---
+++ infer_timecourse_source_GP.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 10 10:57:38 2017
+
+@author: bitzer
+"""
+
+import numpy as np
+import helpers
+import regressors
+import source_visualisations as sv
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+import gc
+import GPy
+
+
+#%% select data
+# source data file to use
+# label mode = mean
+file = 'source_epochs_allsubs_HCPMMP1_201708232002.h5'
+
+# area that should be investigated
+area = '4'
+
+timeslice = [0, 700]
+
+normsrc = True
+
+
+labels = ['{}_{}_ROI-{}h'.format(hemi, area, hemi.lower()) 
+          for hemi in ['L', 'R']]
+
+subjects = helpers.find_available_subjects(megdatadir=helpers.megdatadir)
+
+srcfile = os.path.join(sv.bem_dir, file)
+
+# get mean and std of data across all subjects, trials and times
+if normsrc:
+    epochs_mean = pd.read_hdf(srcfile, 'epochs_mean')
+    epochs_std = pd.read_hdf(srcfile, 'epochs_std')
+
+
+#%% helper for fitting sparse GP
+def fitgp(xvals, data, Z):
+    if data.ndim < 2:
+        data = data[:, None]
+    if xvals.ndim < 2:
+        xvals = xvals[:, None]
+    if Z.ndim < 2:
+        Z = Z[:, None]
+    
+    gpm = GPy.models.SparseGPRegression(xvals, data, Z=Z)
+    
+    # set reasonable initial values
+    gpm.likelihood.variance = data.var()
+    gpm.kern.lengthscale = xvals.std()
+    
+    gpm.optimize()
+    
+    return gpm
+
+
+#%% infer for each subject individually
+choices = regressors.subject_trial.response.loc[subjects]
+
+store = pd.HDFStore(srcfile, 'r')
+
+Z = np.linspace(*timeslice, 15)
+xpred = np.linspace(*timeslice, 200)
+
+gppred = pd.DataFrame([], 
+        index=pd.MultiIndex.from_product(
+                [subjects, labels, xpred], 
+                names=['subject', 'label', 'time']),
+        columns=['mean', 'std'], dtype=float)
+
+for sub in subjects:
+    print('\rsubject = %2d' % sub, end='', flush=True)
+    epochs = store.select('label_tc', 'subject=sub')
+    
+    times = epochs.index.levels[2]
+    times = times[slice(*times.slice_locs(*timeslice))]
+    
+    for hemi in ['L', 'R']:
+        label = '{}_{}_ROI-{}h'.format(hemi, area, hemi.lower())
+        
+        # select trials of the contralateral choice
+        if hemi == 'L':
+            trials = choices.loc[sub] == 1
+        else:
+            trials = choices.loc[sub] == -1
+        trials = list(trials[trials].index)
+        
+        data = epochs.loc[(sub, trials, slice(None)), label]
+        
+        # normalise data
+        if normsrc:
+            data = (data - epochs_mean[label]) / epochs_std[label]
+        
+        # fit the sparse GP
+        gpm = fitgp(data.index.get_level_values('time').values, data.values, Z)
+        
+        # get the noiseless prediction
+        gpmean, gpvar = gpm.predict_noiseless(xpred[:, None])
+        gppred.loc[(sub, label, slice(None)), 'mean'] = gpmean[:, 0]
+        gppred.loc[(sub, label, slice(None)), 'std'] = np.sqrt(gpvar[:, 0])
+        
+    gc.collect()
+        
+
+#%% plot
+colors = sns.cubehelix_palette(2, start=1.3, rot=0, light=.6, dark=.2)
+
+fig, axes = plt.subplots(1, 2, sharex=True, sharey=True, figsize=[7.5, 4.5])
+
+for ax, label in zip(axes, labels):
+    labelpred = gppred.xs(label, level='label').unstack('subject')
+    lsubs = ax.plot(xpred / 1000, labelpred['mean'], color=colors[0], lw=1)
+    
+    # compute mean prediction across subjects taking uncertainty into account
+    weights = (  labelpred['std'].values 
+               / labelpred['std'].sum(axis=1).values[:, None])
+    meanpred = (labelpred['mean'] * weights).sum(axis=1)
+    
+    lmean, = ax.plot(xpred / 1000, meanpred, color=colors[1], lw=3)
+    
+    ax.set_title(label[0])
+    ax.set_xlabel('time from first dot onset (s)')
+
+ax.legend([lsubs[0], lmean], ['participants', 'mean'])
+axes[0].set_ylabel('normalised mean currents in area %s' % area)
+fig.subplots_adjust(top=0.92, right=0.97, left=0.1, wspace=0.08)
