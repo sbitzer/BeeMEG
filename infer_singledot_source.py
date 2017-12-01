04414c1580cf9cf29e2434628328c7a17aa7f82c
Sebastian Bitzer <sebastian.bitzer@tu-dresden.de>
2017-12-01 15:55:58 +0100

    plot dot_y and response effects in the brain + bugfix
    For my DySCo presentation I generated new figures, especially for
    response-related effects. I now explicitly allow the colormap to be generated
    from a different regressor than the chosen one to unify colormaps across
    regressors.
    
    As for dot_y only POS1 becomes significant at 320 or 330 ms, respectively, I
    now also allow that non-significant effects are shown, but with the same
    colormap. For absmean as show_measure this shows a few more effects also at 170
    ms. These must be non-significant, because larger across-subject variance than
    for POS1 around 320 ms.
    
    For the first dot-aligned response-effects no significant results exist after
    FDR. Hence, I choose the colormap based on the quantiles of the selected
    measure. The particular quantiles are chosen to exclude most spurious results
    as per my intuition.
    
    I also made a new analysis for response-aligned response effects for the talk.
    Inference results for that are in
    
    	source_singledot_201711281123.h5 (response-aligned in [-200, 50])
    
    bugfix
    ------
    I forgot that the file names generated by save_image_sequence will
    contain the index into the time vector and not directly the time. Previously,
    this was not an issue, because the times I looked at started at 0 so that 120
    ms corresponded also to index 12, but for response-aligned times index 0 may
    correspond to time -200. Now I account for that when stitching the generated
    files together.

--- infer_singledot_source.py
+++ infer_singledot_source.py
@@ -26,7 +26,7 @@ dots = np.arange(1, 6)
 rt_thresh = -1.0
 
 # names of regressors that should enter the GLM
-r_names = ['dot_x', 'dot_y', 'entropy', 'trial_time', 'intercept', 'response']
+r_names = ['entropy', 'trial_time', 'intercept', 'response']
 R = len(r_names)
 
 # source data to use
@@ -51,14 +51,14 @@ srcfile = os.path.join('mne_subjects', 'fsaverage', 'bem', srcfile)
 # whether a response-aligned analysis should be run, i.e., whether time should
 # be defined with respect to the response time; if True, set timeslice below
 # providing both ends
-response_aligned = False
+response_aligned = True
 
 # time window for which to run the analysis, must be a 0-, 1- or 2-element list
 # if empty list, all times in srcfile are used for analysis;
 # if 1-element, all times starting with the given will be included in analysis;
 # if 2-element, these are the slice limits considered (right edge is inclusive)
 # note that normalisation of data is still over all times in srcfile
-timeslice = []
+timeslice = [-200, 50]
 
 # How many permutations should be computed?
 nperm = 3
--- make_brain_figures.py
+++ make_brain_figures.py
@@ -27,11 +27,28 @@ figdir = os.path.expanduser('~/ZIH/texts/BeeMEG/figures')
 # trialregs_dot=0, source GLM, sum_dot_y, constregs=0 for 1st dot, 
 # subject-specific normalisation of DM without centering and scaling by std
 # label_tc normalised across trials, times and subjects
-basefile = 'source_sequential_201709061827.h5'
+#basefile = 'source_sequential_201709061827.h5'
+
+# label mode = mean, baseline (-0.3, 0), dots 1-5, 
+# source GLM, dot_x, dot_y, trial regressors
+# subject-specific normalisation of DM without centering and scaling by std
+# label_tc normalised across trials, times and subjects
+#basefile = 'source_singledot_201711131931.h5'
+
+# label mode = mean, baseline (-0.3, 0), response-aligned in [-200, 50]
+# source GLM, trial regressors only
+# subject-specific normalisation of DM without centering and scaling by std
+# label_tc normalised across trials, times and subjects
+basefile = 'source_singledot_201711281123.h5.tmp'
 
 
 #%%
-regressors = ['dot_x', 'dot_y', 'accev']
+if basefile.startswith('source_sequential'):
+    regressors = ['dot_x', 'dot_y', 'accev', 'sum_dot_y_prev']
+
+elif basefile.startswith('source_singledot'):
+    regressors = ['response']
+    
 fdr_alpha = 0.01
 
 clusters = ss.get_fdrcorr_clusters(basefile, regressors, fdr_alpha, 
@@ -54,14 +71,66 @@ clusters = ss.get_fdrcorr_clusters(basefile, regressors, fdr_alpha,
 
 
 #%% load specific regressors
-regressors = ['dot_x', 'dot_y', 'accev']
 show_measure = 'absmean'
-r_name = 'dot_x'
+r_name = 'response'
+
+# set 'mask = clusters' to only show significant effects, otherwise set to None
+mask = clusters
 
 use_basefile = show_measure in ss.basefile_measures
 
+if show_measure == 'consistency':
+    colorinfo = {'fmin': 0, 'fmid': 0.5, 'fmax': 1, 'transparent': False,
+                 'colormap': 'coolwarm'}
+    
+    x_times = [400]
+else:
+    if basefile.startswith('source_sequential'):
+        # regressor which should be used to define the colormap
+        r_name_cmap = 'dot_x'
+        
+        # load only the significant data
+        src_df_masked = ss.load_src_df(basefile, r_name_cmap, clusters, 
+                                       use_basefile)
+        if show_measure not in src_df_masked.columns:
+            ss.add_measure(src_df_masked, show_measure)
+        
+        colorinfo = {'fmin': src_df_masked[show_measure].abs().min(),
+                     'fmid': src_df_masked[show_measure].abs().median(), 
+                     'fmax': src_df_masked[show_measure].abs().max(), 
+                     'transparent': True,
+                     'colormap': 'auto'}
+    elif basefile.startswith('source_singledot'):
+        r_name_cmap = 'response'
+        
+        # there are no significant effects after FDR, so don't mask
+        src_df_masked = ss.load_src_df(basefile, r_name_cmap, None, 
+                                       use_basefile)
+        if show_measure not in src_df_masked.columns:
+            ss.add_measure(src_df_masked, show_measure)
+            
+        # set quantile-based colormap info
+        colorinfo = {'fmin': src_df_masked[show_measure].abs().quantile(0.95),
+                     'fmid': src_df_masked[show_measure].abs().quantile(0.99),
+                     'fmax': src_df_masked[show_measure].abs().quantile(0.999),
+                     'transparent': True,
+                     'colormap': 'auto'}
+    
+    if r_name == 'dot_x':
+        x_times = [120, 170, 320, 400]
+    elif r_name == 'accev':
+        x_times = [20, 150, 230, 300]
+        
+#        colorinfo['fmin'] = 0
+        colorinfo['center'] = 0
+    elif r_name == 'dot_y':
+        x_times = [120, 170, 320, 400]
+    elif r_name == 'response':
+        x_times = [780, 820, 890]
+
+# load the selected regressors and mask as desired
 src_df_masked = pd.concat(
-        [ss.load_src_df(basefile, reg, clusters, use_basefile) 
+        [ss.load_src_df(basefile, reg, mask, use_basefile) 
          for reg in regressors],
         keys=regressors, names=['regressor', 'label', 'time'])
 
@@ -70,56 +139,36 @@ times = src_df_masked.index.levels[2]
 if show_measure not in src_df_masked.columns:
     ss.add_measure(src_df_masked, show_measure)
 
-# flip sign of all non-nan values in accev for which there is a nan value
-# in dot_x 100 ms later - these are the effects that are only present in 
-# accumulated evidence, but not in dot_x
-fliptimes = times[times <= times[-1]-100]
-accevvals = src_df_masked.loc[('accev', slice(None), fliptimes), show_measure]
-dotxvals = src_df_masked.loc[('dot_x', slice(None), fliptimes+100), show_measure]
-flip = np.ones(dotxvals.size)
-flip[dotxvals.isnull().values] = -1
-print('number of flips = %d' % np.sum(flip < 0))
-# additionally flip all non-nan values for which the sign of the effect differs
-# note that the meaning of accev is such that its sign is flipped with respect
-# to dot_x (dot_x is large for evidence for right while accev measures evidence
-# for left), this means that you should only those effects are different whose
-# signs are actually equal
-signdiff = (
-          np.sign(src_df_masked.loc[('accev', slice(None), fliptimes), 
-                                    'mean'].values) 
-        - np.sign(src_df_masked.loc[('dot_x', slice(None), fliptimes+100), 
-                                    'mean'].values))
-flip[signdiff == 0] = -1
-print('number of flips with signdiff = %d' % np.sum(flip < 0))
-src_df_masked.loc[('accev', slice(None), fliptimes), show_measure] = accevvals * flip
-
-if show_measure == 'consistency':
-    colorinfo = {'fmin': 0, 'fmid': 0.5, 'fmax': 1, 'transparent': False,
-                 'colormap': 'coolwarm'}
-    
-    x_times = [400]
-else:
-    if r_name == 'dot_x':
-        colorinfo = {'fmin': src_df_masked.loc[r_name, show_measure].abs().min(),
-                     'fmid': src_df_masked.loc[r_name, show_measure].abs().median(), 
-                     'fmax': src_df_masked.loc[r_name, show_measure].abs().max(), 
-                     'transparent': True,
-                     'colormap': 'auto'}
-        
-        x_times = [120, 170, 320, 400]
-    elif r_name == 'accev':
-        x_times = [20, 150, 230, 300]
-        
-        colorinfo = {'fmin': 0,
-                     'fmid': src_df_masked.loc['dot_x', show_measure].abs().median(), 
-                     'fmax': src_df_masked.loc['dot_x', show_measure].abs().max(),
-                     'transparent': True,
-                     'center': 0,
-                     'colormap': 'auto'}
-        
-        colormap = 'auto'
+if basefile.startswith('source_sequential'):
+    # flip sign of all non-nan values in accev for which there is a nan value
+    # in dot_x 100 ms later - these are the effects that are only present in 
+    # accumulated evidence, but not in dot_x
+    fliptimes = times[times <= times[-1]-100]
+    accevvals = src_df_masked.loc[('accev', slice(None), fliptimes), show_measure]
+    dotxvals = src_df_masked.loc[('dot_x', slice(None), fliptimes+100), show_measure]
+    flip = np.ones(dotxvals.size)
+    flip[dotxvals.isnull().values] = -1
+    print('number of flips = %d' % np.sum(flip < 0))
+    # additionally flip all non-nan values for which the sign of the effect differs
+    # note that the meaning of accev is such that its sign is flipped with respect
+    # to dot_x (dot_x is large for evidence for right while accev measures evidence
+    # for left), this means that you should only those effects are different whose
+    # signs are actually equal
+    signdiff = (
+              np.sign(src_df_masked.loc[('accev', slice(None), fliptimes), 
+                                        'mean'].values) 
+            - np.sign(src_df_masked.loc[('dot_x', slice(None), fliptimes+100), 
+                                        'mean'].values))
+    flip[signdiff == 0] = -1
+    print('number of flips with signdiff = %d' % np.sum(flip < 0))
+    src_df_masked.loc[('accev', slice(None), fliptimes), show_measure] = accevvals * flip
 
 hemi = 'rh'
+
+if mask is None:
+    filepat_base = 'brain_unmasked_' + show_measure
+else:
+    filepat_base = 'brain_' + show_measure
 
 
 #%% make helper plotting function
@@ -138,8 +187,8 @@ def brain_plot(r_name, viewtimes, brain):
     sv.show_labels_as_data(src_df_masked.loc[r_name], show_measure, 
                            brain, **colorinfo)
     
-    filepat = os.path.join(figdir, 'brain_{}_{}_{}_%02d.png'.format(
-            show_measure, hemi, r_name))
+    filepat = os.path.join(figdir, filepat_base + '_{}_{}_%02d.png'.format(
+            hemi, r_name))
     
     brain.save_image_sequence([times.get_loc(t) for t in viewtimes],
                               filepat, montage=views[hemi])
@@ -156,6 +205,7 @@ def list_effects(time):
     effects = pd.DataFrame(
             src_df_masked.loc[(r_name, slice(None), time), show_measure]
             .dropna().sort_values())
+    effects = effects[effects[show_measure].abs() > colorinfo['fmin']]
     effects.reset_index(level='label', inplace=True)
     effects.reset_index(drop=True, inplace=True)
     effects['region'] = list(map(ss.get_Glasser_section, effects.label))
@@ -166,60 +216,14 @@ def list_effects(time):
 #%% stitch images from different hemispheres together
 infiles = []
 for time in x_times:
+    time_idx = times.get_loc(time)
     infiles += [
             os.path.join(figdir, 
-                         'brain_{}_{}_{}_{:02.0f}.png'.format(
-                                 show_measure, 'lh', r_name, time/10)),
+                         filepat_base + '_{}_{}_{:02d}.png'.format(
+                                 'lh', r_name, time_idx)),
             os.path.join(figdir, 
-                         'brain_{}_{}_{}_{:02.0f}.png'.format(
-                                 show_measure, 'rh', r_name, time/10))]
-outfile = os.path.join(figdir, 'brain_{}_{}.png'.format(show_measure, r_name))
+                         filepat_base + '_{}_{}_{:02d}.png'.format(
+                                 'rh', r_name, time_idx))]
+outfile = os.path.join(figdir, filepat_base + '_{}.png'.format(r_name))
 os.system("montage -tile 2x{:d} -geometry +0+0 {} {}".format(
         len(x_times), ' '.join(infiles), outfile))
-
-
-#%% get threshold and clusters for trial regressors of interest
-#regressors = ['response']
-#
-#threshold, measure_cdf = ss.find_slabs_threshold(
-#    basefile, measure, quantile=1-alpha, regressors=regressors, 
-#    verbose=1, return_cdf=True, exclude='dotregs')
-#
-## identify significant clusters
-#clusters = ss.get_fdrcorr_clusters(basefile, regressors, measure, threshold, 
-#                                   measure_cdf, fdr_alpha=0.001)
-#
-#
-##%% plot effects for response
-#r_name = 'response'
-#show_measure = 'consistency'
-#t_off = 200
-#
-#src_df_masked = pd.concat(
-#        [ss.load_src_df(basefile, reg, clusters) for reg in regressors],
-#        keys=regressors, names=['regressor', 'label', 'time'])
-#
-#times = src_df_masked.index.levels[2] + t_off
-#
-## update time to trial time
-#src_df_masked.index = pd.MultiIndex.from_product(
-#        src_df_masked.index.levels[:2] + [times], 
-#        names=['regressor', 'label', 'time'])
-#
-#if show_measure == 'consistency':
-#    colorinfo = [0, 0.5, 1, False]
-#    colormap = 'coolwarm'
-#    clim = {'kind': 'value', 'lims': [0, 0.5, 1]}
-#else:
-#    colorinfo = [src_df_masked[show_measure].min(),
-#                 src_df_masked[show_measure].median(), 
-#                 src_df_masked[show_measure].max(), 
-#                 True]
-#    colormap = 'auto'
-#    clim = 'auto'
-#
-#r_times = [320, 780, 880]
-#
-#for hemi in ['lh', 'rh']:
-#    brain = brain_plot(r_name, r_times, hemi, colormap=colormap, 
-#                       clim=clim)
