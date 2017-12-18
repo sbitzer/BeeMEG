50a5532f0d2e62bd8e610206d9c566ef9c8eb423
Sebastian Bitzer <sebastian.bitzer@tu-dresden.de>
2017-12-18 11:05:30 +0100

    linearity only as global parameter + approximate initialisation
    Removing the subject-level linearity parameters solved the potential inference
    problems, i.e., Rhat is equal to 1.0 and neff is close to 2000 for almost all
    sampled parameters in the model without subject-level linearity parameters.
    
    Inference, however, is relatively slow, especially in the initial iterations.
    It took around 350 s for all chains to sample 2000 samples in my initial
    tests and it took a while before the first 100 iterations were finished, but
    from about 500 iterations most chains produce samples quite fast. So I
    suspected that bad initialisation of the sampling could lead to these early
    delays and devised a sensible data-based initialisation scheme. I only ran this
    once so far, but it didn't lead to considerably faster sampling (total time
    until all chains converged was 380 s) such that I believe that I can stay with
    the standard random initialisation.

--- infer_linearity.py
+++ infer_linearity.py
@@ -99,16 +99,12 @@ parameters {
     vector[S] beta_diff_norm;
     real<lower=0> std_beta;
     
-    real<lower=0> std_l;
-    vector<lower=-l/std_l>[S] l_diff_norm;
-    
     vector[S] intercept;
     vector<lower=0>[S] stdy;
 }
 
 transformed parameters {
     vector[S] beta_sub = beta + beta_diff_norm * std_beta;
-    vector[S] l_sub = l + l_diff_norm * std_l;
 }
 
 model {
@@ -122,15 +118,12 @@ model {
     beta_diff_norm ~ normal(0, 1);
     std_beta ~ exponential(1);
     
-    l_diff_norm ~ normal(0, 1);
-    std_l ~ exponential(1);
-    
     intercept ~ normal(0, 1);
     stdy ~ exponential(1);
     
     for (sub in 1:S) {
         mu[n+1 : n+N[sub]] = beta_sub[sub] * 2 * (
-                inv_logit(x[n+1 : n+N[sub]] / l_sub[sub]) - 0.5)
+                inv_logit(x[n+1 : n+N[sub]] / l) - 0.5)
                 + intercept[sub];
         
         std[n+1 : n+N[sub]] = rep_vector(stdy[sub], N[sub]);
@@ -195,11 +188,16 @@ y = []
 for sub in range(S):
     x.append(np.random.randn(Nsub[sub]))
     
-#    mu = beta_sub[sub] / 3 * x[-1]
     mu = beta_sub[sub] * np.sign(x[-1])
     
     y.append(np.random.randn(Nsub[sub]) * sigmas[sub] + mu)
+
+ylin = []
+for sub in range(S):
+    mu = beta_sub[sub] / 3 * x[sub]
     
+    ylin.append(np.random.randn(Nsub[sub]) * sigmas[sub] + mu)
+
 data = {'S': S, 'N': Nsub, 'Nall': Nsub.sum(),
         'x': np.concatenate(x), 'y': np.concatenate(y)}
 
@@ -210,7 +208,64 @@ def get_xy(xy, sub):
     return data[xy][Nsub[:sub].sum():Nsub[:sub+1].sum()]
 
 
+#%% reasonable initialisation to speed things up a little (speed-up effect may
+# be little to non-existent)
+def get_hierarchical_init():
+    init = {}
+    
+    # intercept is within-subject mean
+    init['intercept'] = np.array([ysub.mean() for ysub in y])
+    
+    # stdy is within-subject std
+    init['stdy'] = np.array([ysub.std() for ysub in y])
+    
+    # randomise intercept using its standard error
+    init['intercept'] += np.random.randn(S) * init['stdy'] / np.sqrt(S-1)
+    
+    # randomise stdy using its across-subject std
+    init['stdy'] = np.abs(
+            init['stdy'] + np.random.randn(S) * init['stdy'].std())
+    
+    # l somewhere between 0 and 1
+    init['l'] = np.random.rand()
+    
+    # beta estimated from overall correlation (* 3, because the correlation is 
+    # the linear slope and the range of the normalised x should be roughly
+    # within [-3, 3])
+    est_beta = lambda x, y: np.abs(np.corrcoef(x, y)[0, 1]) * 3
+    init['beta'] = est_beta(data['y'], data['x'])
+    
+    # subject-specific beta-estimates
+    beta_sub = np.array([est_beta(xsub, ysub) for xsub, ysub in zip(x, y)])
+    
+    # std_beta as variability in betasub
+    init['std_beta'] = np.std(beta_sub - init['beta'])
+    
+    # beta_diff_norm estimated from difference between overall correlation and 
+    # subject-specific correlation
+    init['beta_diff_norm'] = (beta_sub - init['beta']) / init['std_beta']
+    
+    # randomise beta within across-subject variability of betas
+    init['beta'] = np.abs(init['beta'] + np.random.randn() * beta_sub.std())
+    
+    # randomise beta_diff_norm within its own variability
+    init['beta_diff_norm'] = np.abs(
+            init['beta_diff_norm'] + np.random.randn(S) * init['std_beta'])
+    
+    # randomise std_beta within its standard error as approximated from here:
+    # https://stats.stackexchange.com/questions/156518/what-is-the-standard-error-of-the-sample-standard-deviation
+    # assuming a normal distribution
+    init['std_beta'] = np.abs(
+            init['std_beta'] + np.random.randn() 
+            * np.sqrt(2 * init['std_beta'] ** 4 / (S-1)))
+    
+    return init
+
+
 #%% 
 sm = pystan.StanModel(model_code=hierarchical_step_stan, verbose=True)
 
-fit = sm.sampling(data, iter=1000, chains=4)
+fit = sm.sampling(data, iter=1000, chains=4, init=get_hierarchical_init)
+
+with open('fitres.txt', 'w') as file:
+    print(fit, file=file)
