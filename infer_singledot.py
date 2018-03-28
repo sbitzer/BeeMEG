941092b76e53e488d2a3cd462e5ddad84c261d6d
Sebastian Bitzer <sebastian.bitzer@tu-dresden.de>
2018-03-28 13:48:16 +0200

    common singledot inference for sensors and sources + new results
    For the paper I needed to update the singledot analysis on the sensors with new
    developments that I had created in the meantime (normalisation of DM and data,
    exclude timeouts explicitly). I took this as occassion to create a common
    inference script for both types of data, as done in infer_sequential.py. I
    actually used infer_sequential as a template, but, e.g., moved most data
    manipulations out of the time loop, because they could be done independent of
    time or across all times at once. I also included all options from
    infer_singledot_source.py, especially the possibility to do a response-aligned
    analysis (I did not test this, yet, but as I just copied the code, it should
    work).
    
    I ran an analysis for the first 6 dots up to 1200 ms after first dot onset and
    chose other options in line with the other analyses in the paper (local
    normalisation of DM, trial normalisation of data, exclude time outs, toolate =
    -200). Other than in previous analyses I now also exclude coefficients of
    subjects on the second level when DM condition number for the subject was >10,
    but this criterion is very similar to requiring that the coefficients had to be
    computed from at least 30 data points: for sub 9 only the last time point had
    cn>10 although num data was 31, for sub 24 28 times were excluded based on num
    data while only 8 of those had cn>10, for sub 32 one more time point was
    excluded based on cn>10 than based on num data (41) and for sub 33 both
    criteria excluded the same 29 trials.
    
    For the paper plot I now select the magnetometer channel with the largest
    average absolute second-level t-value across time points. For illustration I
    now also plot the topogrphy of these values.
    
    Results can be found in:
    	meg_singledot_201803271303.h5
    
    They look very similar to what I had seen before. I didn't check anything else.

---
+++ infer_singledot.py
@@ -0,0 +1,445 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 26 14:38:01 2018
+
+@author: bitzer
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 22 15:37:21 2017
+
+@author: bitzer
+"""
+
+import numpy as np
+import helpers
+import pandas as pd
+import subject_DM
+from scipy.stats import ttest_1samp
+import os, gc, re
+import statsmodels.api as sm
+from warnings import warn
+
+
+#%% options
+# which dots to investigate? Note that the more dots you include, the fewer
+# trials will be available in the regression, if you exclude trials based on
+# toolate below
+dots = np.arange(1, 7)
+
+# allows you to exclude trials based on response-aligned time in dot 
+# onset aligned analysis: all trials with response-aligned time >= toolate will
+# be removed from regression analysis, set to 5000 or higher to include all 
+# available trials
+toolate = -200
+
+# exclude timed-out trials
+exclude_to = True
+
+# only include subjects in second-level summary statistics analysis for who
+# the number of trials available at the considered time point is at least:
+mindata = 30
+
+# names of regressors that should enter the GLM
+r_names = ['dot_x', 'dot_y', 'entropy', 'trial_time', 'intercept', 'response']
+R = len(r_names)
+# sort for use in index below
+r_names.sort()
+# get associated regressor categories
+r_cats = subject_DM.regressors.get_regressor_categories(r_names)
+
+# whether a response-aligned analysis should be run, i.e., whether time should
+# be defined with respect to the response time; if True, set timeslice below
+# providing both ends
+response_aligned = False
+
+# time window for which to run the analysis, must be a 0-, 1- or 2-element list
+# if empty list, all times in srcfile are used for analysis;
+# if 1-element, all times starting with the given will be included in analysis;
+# if 2-element, these are the slice limits considered (right edge is inclusive)
+# note that global normalisation of data is still over all times in srcfile
+timeslice = [0, 1200]
+
+# How many permutations should be computed?
+nperm = 3
+
+# set to 'local' for normalisation of DM within each t0, i.e., locally for each
+#   regression that is run - means that each beta can be interpreted as the 
+#   correlation value describing how strongly the regressor is represented in
+#   the data at that time point and allows more or less fair comparison of 
+#   regressor betas within and across each time point
+# set to 'subject' to normalise the DM within each subject before entries are
+#   excluded based on RTs or non-availability of data - means that betas 
+#   reflect how strongly the regressor is represented in the data in relation
+#   to the overall scale of the regressor; for regressors with theoretically
+#   constant scale across dots (dots as independent samples from the same
+#   distribution) this should lead to more accurate estimation of regressor
+#   scale for normalisation, but if the scale of regressors is expected to
+#   change across dots resulting betas may not be comparable; for example,
+#   the scale of accumulated evidence rises with dot index meaning that at low
+#   t0 regressor values will include more high values than at high t0s (because
+#   for high t0s we exclude late dots from the analysis as they fall out of the
+#   epoch time window, or are too late in relation to RT); if the true relation
+#   between the regressor and the signal is linear and constant across time 
+#   points, the estimated betas should be comparable, but if, for example, 
+#   the relation to accev follows more a sigmoid where large values are mapped
+#   to similar, moderately high signal values, the betas for early t0s may be
+#   lower than the ones for late t0s, although it doesn't mean that accev is
+#   less represented in the signal early after dot onset
+normDM = 'local'
+
+# set to 'global' to normalise the data to have mean 0 and std 1 in 
+#   each data label across trials, time points and subjects
+# set to 'trials' to normalise to mean 0, std 1 within each subject and time
+#   point, i.e., normalise only across trials; together with normDM=local 
+#   allows to interpret estimated coefficients as correlations
+normdata = 'trials'
+
+# data to use, 'meg' for MEG channels, 'source' for sources
+datatype = 'meg'
+
+if datatype == 'meg':
+    sfreq = 100
+    megtype = 'mag'
+    eog = False
+    bl = (-0.3, 0)
+    window = [0, 2.5]
+    
+    srcfile = helpers.create_meg_epochs_hdf(sfreq, sfreq, window, megtype, bl,
+                                            eog=eog)
+    epname = 'epochs'
+    
+elif datatype == 'source':
+    # label mode = mean, long epochs, HCPMMP_5_8
+    srcfile = 'source_epochs_allsubs_HCPMMP1_5_8_201712061743.h5'
+    
+    # label mode = mean
+    #srcfile = 'source_epochs_allsubs_HCPMMP1_201708232002.h5'
+    
+    # label mode = mean_flip
+    #srcfile = 'source_epochs_allsubs_HCPMMP1_201706131725.h5'
+    
+    # label mode= None, lselection=[V1, V2], window=[0, 0.4]
+    #srcfile = 'source_epochs_allsubs_HCPMMP1_201708151431.h5'
+    
+    # label mode = (abs) max
+    #srcfile = 'source_epochs_allsubs_HCPMMP1_201706271717.h5'
+    
+    # label mode= None, lselection=pre-motor and motor areas, window=[0.3, 0.9]
+    #srcfile = 'source_epochs_allsubs_HCPMMP1_201710231731.h5'
+    
+    # label mode = None, lselection=premotor, motor, mid cingulate, parietal
+    # window = [-0.3, 2.5], no baseline correction before source reconstruction
+#    srcfile = 'source_epochs_allsubs_HCPMMP1_5_8_201802081827.h5'
+    
+    srcfile = os.path.join('mne_subjects', 'fsaverage', 'bem', srcfile)
+    epname = 'label_tc'
+
+# where to store
+file = pd.datetime.now().strftime(datatype+'_singledot'+'_%Y%m%d%H%M'+'.h5')
+# tmp-file should use local directory (preventing issue with remote store)
+tmpfile = os.path.join('/media/bitzer/Data', file+'.tmp')
+file = os.path.join(helpers.resultsdir, file)
+
+# create HDF5-file and save chosen options
+with pd.HDFStore(file, mode='w', complevel=7, complib='blosc') as store:
+    store['scalar_params'] = pd.Series(
+            [toolate, exclude_to, mindata, float(response_aligned)], 
+            ['toolate', 'exclude_to', 'mindata', 'response_aligned'])
+    store['dots'] = pd.Series(dots)
+    store['normDM'] = pd.Series(normDM)
+    store['normdata'] = pd.Series(normdata)
+    store['srcfile'] = pd.Series(srcfile)
+
+
+#%% extract some basic info from example data
+subjects = helpers.find_available_subjects(megdatadir=helpers.megdatadir)
+S = subjects.size
+
+with pd.HDFStore(srcfile, 'r') as store:
+    epochs = store.select(epname, 'subject=2')
+
+# times stored in epochs
+epochtimes = epochs.index.levels[2]
+
+# trials available in data (should be the same as in DM)
+trials = epochs.index.get_level_values('trial').unique()
+
+if normdata == 'global':
+    # get mean and std of data across all subjects, trials and times
+    epochs_mean = pd.read_hdf(srcfile, 'epochs_mean')
+    epochs_std = pd.read_hdf(srcfile, 'epochs_std')
+
+gc.collect()
+
+
+#%% load rts and choices of subjects
+rts = subject_DM.regressors.subject_trial.RT.loc[subjects] * 1000
+
+# if RTs are an exact multiple of 5, numpy will round both 10+5 and 20+5 to
+# 20; so to not get duplicate times add a small jitter
+fives = (np.mod(rts, 10) != 0) & (np.mod(rts, 5) == 0)
+# subtracting the small amount means that the new times will be rounded
+# down so that e.g., -5 becomes 0, I prefer that round because then there
+# might be slightly more trials with larger times, but the effect is small
+rts[fives] = rts[fives] - 1e-7
+
+choices = subject_DM.regressors.subject_trial.response.loc[subjects]
+
+
+#%% figure out which time points to analyse
+if response_aligned:
+    def rtindex(sub):
+        """Returns index for data of a subject with time as difference to RT"""
+        
+        rttime = (  epochtimes.values[None, :] 
+                  - rts.loc[sub].values[:, None]).flatten()
+        # round to 10 ms and convert into integer
+        rttime = rttime.round(-1).astype(int)
+    
+        # create new subject-trial-time index (I cannot only set the labels of
+        # the time level, because the response aligned times have different 
+        # values than those stored in dot-onset aligned times)
+        return pd.MultiIndex.from_arrays(
+                [np.full_like(rttime, sub),
+                 np.tile(np.arange(1, 481)[:, None], 
+                         (1, epochtimes.size)).flatten(),
+                 rttime], names=['subject', 'trial', 'time']).sort_values()
+    
+    if len(timeslice) != 2:
+        raise ValueError("For response aligned times you need to provide the "
+                         "start and stop of the time window!")
+    times = pd.Index(np.arange(timeslice[0], timeslice[1]+10, step=10), 
+                     name='time')
+else:
+    times = epochtimes[slice(*epochtimes.slice_locs(*timeslice))]
+
+
+#%% load trial-level design matrices for all subjects (ith-dot based)
+DM = subject_DM.get_trial_DM(dots, subjects, r_names=r_names)
+
+# add selected single dot regressors (if you specifically requested, e.g., 
+# dot_x_9 in r_names)
+single_r_names = []
+single_r_names_components = []
+for r_name in r_names:
+    match = re.match(r'(.*)_(\d)$', r_name)
+    if match:
+        single_r_names.append(r_name)
+        single_r_names_components.append(match.groups())
+
+DM = pd.concat(
+        [DM]+[subject_DM.get_trial_DM(int(dot), subjects, r_names=[r_name]) 
+              for r_name, dot in single_r_names_components], axis=1)
+
+# move_dist_1 is constant at 0, we don't need to fit that
+if 'move_dist' in r_names:
+    del DM['move_dist_1']
+    
+# sort regressor names (columns) for indexing
+DM.sort_index(axis=1, inplace=True)
+
+# normalise within subject, if desired;
+# also centers the regressors, but only if their mean exceeds the standard 
+# deviation by a given factor (2 by default)
+if normDM == 'subject':
+    DM = subject_DM.normalise_DM(DM, 'subject')
+
+if DM.isnull().any().any():
+    raise ValueError("Design matrix has NaNs somewhere, please check!")
+
+
+#%% prepare output and helpers
+datalabels = epochs.columns
+
+first_level = pd.DataFrame([], 
+        index=pd.MultiIndex.from_product([np.arange(nperm+1), 
+              datalabels, times], 
+              names=['permnr', 'label', 'time']),
+        columns=pd.MultiIndex.from_product([subjects, ['beta', 'bse'], 
+              DM.columns], names=['subject', 'measure', 'regressor']), 
+              dtype=np.float64)
+first_level.sort_index(axis=1, inplace=True)
+
+first_level_diagnostics = pd.DataFrame([], 
+        index=pd.MultiIndex.from_product([np.arange(nperm+1), 
+              datalabels, times], 
+              names=['permnr', 'label', 'time']),
+        columns=pd.MultiIndex.from_product([subjects, ['Fval', 'R2', 'llf']], 
+              names=['subject', 'measure']), dtype=np.float64)
+first_level_diagnostics.sort_index(axis=1, inplace=True)
+
+second_level = pd.DataFrame([], 
+        index=pd.MultiIndex.from_product([np.arange(nperm+1), 
+              datalabels, times], 
+              names=['permnr', 'label', 'time']), 
+        columns=pd.MultiIndex.from_product([['mean', 'std', 'tval', 'mlog10p'], 
+              DM.columns], names=['measure', 'regressor']), dtype=np.float64)
+second_level.sort_index(axis=1, inplace=True)
+
+assert np.all(first_level.columns.levels[2] == DM.columns), 'order of ' \
+    'regressors in design matrix should be equal to that of results DataFrame'
+assert np.all(second_level.columns.levels[1] == DM.columns), 'order of ' \
+    'regressors in design matrix should be equal to that of results DataFrame'
+
+data_counts = pd.DataFrame(
+        [], index=times, dtype=float,
+        columns=pd.Index(subjects, name='subject'))
+
+DM_condition_numbers = data_counts.copy()
+
+
+#%% infer
+# original sequential order
+permutation = np.arange(trials.size * times.size).reshape(
+        [trials.size, times.size])
+
+for perm in range(1, nperm+1):
+    print('\npermutation %d' % perm)
+    print('-------------')
+    if perm > 0:
+        # randomly permute trials, use different permutations for time points, but use
+        # the same permutation across all subjects such that only trials are permuted,
+        # but random associations between subjects are maintained
+        for t in range(times.size):
+            # shuffles axis=0 inplace
+            np.random.shuffle(permutation[:, t])
+    
+    for sub in subjects:
+        with pd.HDFStore(srcfile, 'r') as store:
+            epochs = store.select(epname, 'subject=sub').loc[sub]
+        gc.collect()
+        
+        # normalise each label, if desired
+        if normdata == 'global':
+            epochs = (epochs - epochs_mean) / epochs_std
+        elif normdata == 'trials':
+            epochs = epochs.unstack('time')
+            epochs = (epochs - epochs.mean()) / epochs.std()
+            epochs = epochs.stack('time')
+        
+        # NaN all values for trials that were timed out, if desired;
+        # only necessary for one column as later rows are excluded based on
+        # nan in any column
+        if exclude_to:
+            ch_sub = choices.loc[sub]
+            epochs.loc[ch_sub[ch_sub == helpers.toresponse[0]].index.values, 
+                      epochs.columns[0]] = np.nan
+                       
+        # NaN within each trial time points that are "too late" with respect to
+        # response aligned time
+        for tr in trials:
+                # small offset added to ensure that only times > RT removed
+                epochs.loc[
+                        (tr, slice(rts.loc[(sub, tr)] + toolate + 1e-6, None)), 
+                        epochs.columns[0]] = np.nan
+                        
+        # change to response-aligned times, when desired
+        if response_aligned:
+            epochs.index = rtindex(sub)
+            
+        # select desired times
+        if len(timeslice) > 0:
+            epochs = epochs.loc[(slice(None), times), :].copy()
+            
+        # permute by reordering rows according to sampled permutation
+        epochs.loc[:] = epochs.values[permutation.flatten(), :]
+        
+        # drop nans to implement exclusion of undesired data points
+        epochs.dropna(inplace=True)
+        
+        # save data counts for this subject
+        if perm == 0:
+            data_counts[sub] = epochs.iloc[:, 0].groupby('time').count()
+        
+        for t0 in times:
+            print('\rsubject = %2d, t0 = % 5d' % (sub, t0), end='', flush=True)
+            
+            data = epochs.xs(t0, level='time')
+            DMt0 = DM.loc[sub].loc[data.index]
+            
+            # normalise DM only across given trials, if desired
+            if normDM == 'local':
+                DMt0 = subject_DM.normalise_DM(DMt0, True)
+            
+            # save data count and condition number
+            if perm == 0:
+                # numbers from 10 indicate some problem with 
+                # collinearity in design matrix (see Rawlings1998, p. 371)
+                # when motoprep is included, high condition numbers at early 
+                # time points (<100 ms) result from motoprep only having very 
+                # few non-zero values due to late responses of some subjects
+                DM_condition_numbers.loc[t0, sub] = (
+                        subject_DM.compute_condition_number(DMt0))
+                
+                if DM_condition_numbers.loc[t0, sub] > 10:
+                    warn("High condition number of design matrix detected "
+                         "({:5.2f}) for subject {:d}, t0 = {:d}!".format(
+                                 DM_condition_numbers.loc[t0, sub], sub, t0))
+            
+            # run regression for each data label, but only for data sets
+            # with at least as many data points as there are regressors
+            if data.shape[0] >= DMt0.shape[1]:
+                for label in datalabels:
+                    res = sm.OLS(data[label].values, 
+                                 DMt0.values, hasconst=True).fit()
+                
+                    first_level.loc[(perm, label, t0), 
+                                    (sub, 'beta', slice(None))] = res.params
+                    first_level.loc[(perm, label, t0), 
+                                    (sub, 'bse', slice(None))] = res.bse
+                    
+                    first_level_diagnostics.loc[(perm, label, t0), 
+                                                (sub, 'Fval')] = res.fvalue
+                    first_level_diagnostics.loc[(perm, label, t0), 
+                                                (sub, 'R2')] = res.rsquared
+                    first_level_diagnostics.loc[(perm, label, t0), 
+                                                (sub, 'llf')] = res.llf
+    
+    print('\ncomputing second level ...')
+    for label in datalabels:
+        for t0 in times:
+            params = first_level.loc[(perm, label, t0), 
+                                     (slice(None), 'beta', slice(None))]
+            params = params.unstack('regressor')
+            
+            # exclude subjects with less than mintrials trials at t0 or 
+            # DM condition numbers greater 10
+            params = params[(data_counts.loc[t0] >= mindata).values &
+                            (DM_condition_numbers.loc[t0] <= 10).values]
+            
+            second_level.loc[(perm, label, t0), ('mean', slice(None))] = (
+                    params.mean().values)
+            second_level.loc[(perm, label, t0), ('std', slice(None))] = (
+                    params.std().values)
+            tvals, pvals = ttest_1samp(params.values, 0, axis=0)
+            second_level.loc[(perm, label, t0), ('tval', slice(None))] = (
+                tvals)
+            second_level.loc[(perm, label, t0), ('mlog10p', slice(None))] = (
+                    -np.log10(pvals))
+            
+    try:
+        with pd.HDFStore(tmpfile, mode='w') as store:
+            store['second_level'] = second_level
+            store['first_level'] = first_level
+            store['first_level_diagnostics'] = first_level_diagnostics
+            store['data_counts'] = data_counts
+            store['DM_condition_numbers'] = DM_condition_numbers
+    except:
+        pass
+        
+try:
+    with pd.HDFStore(file, mode='r+', complevel=7, complib='blosc') as store:
+        store['second_level'] = second_level
+        store['first_level'] = first_level
+        store['first_level_diagnostics'] = first_level_diagnostics
+        store['data_counts'] = data_counts
+        store['DM_condition_numbers'] = DM_condition_numbers
+except:
+    warn('Results not saved yet!')
+else:
+    os.remove(tmpfile)
+    
--- make_singledot_figures.py
+++ make_singledot_figures.py
@@ -8,16 +8,19 @@ Created on Tue Sep 19 14:16:54 2017
 
 import helpers
 import pandas as pd
+import numpy as np
 import os
 import matplotlib.pyplot as plt
-
-figdir = os.path.expanduser('~/ZIH/texts/BeeMEG/figures')
 
 
 #%% load data
 
 # baseline = (-0.3, 0)
 #fname = 'meg_singledot_201706131051.h5'
+
+# magnetometers, baseline = (-0.3, 0), trial normalisation of data, 
+# dots 1-6, local normalisation of DM, exclude timeouts, toolate=-200
+fname = 'meg_singledot_201803271303.h5'
 
 # label mode = mean, baseline (-0.3, 0), dots 3-5, 
 # source GLM, sum_dot_y_prev_3, accev_3, dot_x, dot_y, trial regressors
@@ -35,7 +38,7 @@ figdir = os.path.expanduser('~/ZIH/texts/BeeMEG/figures')
 # source GLM, dot_x, dot_y, trial regressors
 # subject-specific normalisation of DM without centering and scaling by std
 # label_tc normalised across trials, times and subjects
-fname = 'source_singledot_201801171913.h5'
+#fname = 'source_singledot_201801171913.h5'
 
 resfile = os.path.join(helpers.resultsdir, fname)
 
@@ -48,27 +51,50 @@ perms = second_level.index.levels[0]
 dots = pd.read_hdf(resfile, 'dots')
 
 
-#%% plot example signal
+#%% determine label with strongest / most consistent correlations
 r_name = 'dot_x'
-nperm = 3
 regs = ['{}_{}'.format(r_name, d) for d in dots]
 R = len(regs)
 
+avabst = second_level.loc[0, ('tval', regs)].abs().mean(level='label').mean(
+        axis=1)
+
+maxlabel = avabst.idxmax()
+
+
+#%% plot topography of average t-val magnitudes
+evoked.data[:, 0] = avabst.values
+
+fig = evoked.plot_topomap(times=[0], vmin=avabst.min(), vmax=avabst.max(),
+                          scalings={'mag': 1}, units='t', time_format='',
+                          title='x-coordinate\n(average magnitude)',
+                          mask=np.tile((avabst.index == maxlabel)[:, None], 
+                                       (1, evoked.data.shape[1])),
+                          mask_params={'markersize': 10});
+
+fig.savefig(os.path.join(helpers.figdir, 'singledot_mean_abs_t_%s' % r_name),
+            dpi=300)
+
+
+#%% plot example signal
+nperm = 3
+
 measure = 'mean'
-#sensor = 'MEG0741'
-sensor = 'L_4_ROI-lh'
+label = maxlabel
+#label = 'MEG0741'
+#label = 'L_4_ROI-lh'
 
 fig, axes = plt.subplots(R, 1, sharex=True, sharey=True, figsize=[6, 8])
-axes[0].set_title('x-coordinate (%s)' % sensor)
+axes[0].set_title('x-coordinate (%s)' % label)
 
 times = second_level.index.levels[2]
 for r, reg in enumerate(regs):
     line, = axes[r].plot(
-            times, second_level.loc[(0, sensor, slice(None)), (measure, reg)])
+            times, second_level.loc[(0, label, slice(None)), (measure, reg)])
     for p in range(1, nperm+1):
         pl, = axes[r].plot(
                 times, 
-                second_level.loc[(p, sensor, slice(None)), (measure, reg)],
+                second_level.loc[(p, label, slice(None)), (measure, reg)],
                 ':', color=line.get_color(), lw=1)
     axes[r].set_ylabel('dot %s' % reg[-1])
     
@@ -85,7 +111,7 @@ axes[0].set_ylim(yl)
 axes[-1].legend([line, pl, onsetl], ['data', 'permuted', 'dot onset'])
 axes[-1].set_xlabel('time (ms)')
 
-fig.subplots_adjust(left=0.1, bottom=0.06, right=0.95, top=0.96, hspace=0.18)
+fig.tight_layout()
 
-#fig.savefig(os.path.join(figdir, 'singledot_%s_%s.png' % (r_name, sensor)), 
-#            dpi=300)
+fig.savefig(os.path.join(helpers.figdir, 'singledot_%s_%s.png' % (r_name, label)), 
+            dpi=300)
