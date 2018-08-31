c2647cb2274ce108929591ed5f6b9dd245d02518
Sebastian Bitzer <sebastian.bitzer@tu-dresden.de>
2018-08-31 18:43:24 +0200

    major overhaul of the brain difference figure(s)
    This is now all based on the windowed regression results and I first produce
    figures showing the effects in the two time windows from this analysis. For the
    difference figure itself I tried a few different ways of normalising the
    magnitudes, but settled on the theoretically cleanest version as captured in
    helpers.normalise_magnitudes. Additionally, I decided to compute the
    normalisation only on brain areas with significant effects in the corresponding
    time window, because otherwise the majority of brain areas with small effects
    dominate the normalisation and make it harder to investigate the differences in
    spatial activation patterns. Afterall I was mainly interested to see which
    brain areas are activated in the one, but not the other time window.
    
    I decreased fdr_alpha from 0.05 to 0.01, because I had a hard time interpreting
    the (weak) significant activations for correlations that were larger in the
    build-up window. These areas were: right - TPOJ1, MST, V7, 9p, 52, p24, 7PL,
    EC; left - 7Pm, V3CD. So there are temporal, parietal, frontal and cingulate
    areas which did not necessarily occur in the other analyses. From these, the
    occipto-temporal and parietal activations make kind of sense, but what with the
    others and why some in the left and some in the right hemisphere? So it is
    likely that some of these are false positives, but which ones? Unfortunately
    none survived fdr_alpha=0.01 to tell. So I rather stick with leaving the
    question open in the paper, or rather only interpreting the stronger
    differences favouring effects around the response time.
    
    I stitched the three generated figures manually together using Gimp
    (win_brain_response.xcf).

--- make_brain_difference_figure.py
+++ make_brain_difference_figure.py
@@ -33,59 +33,200 @@ r_name = 'response'
 if r_name == 'response':
     # loose source orientations, but cortex normal currents
     # baseline (-0.3, 0), response-aligned
-    # time [-1000, 500], exclude time-outs
-    # trial_time, intercept, response
+    # timewins build-up: [-500, -120], response: [-30, 100], 
+    # exclude time-outs, trial_time, intercept, response
     # trial normalisation of data, local normalisation of DM
-    basefile = 'source_singledot_201808171756.h5'
+    basefile = 'source_singledot_201808291410.h5'
+
+show_measure = 'abstval'
+
+fdr_alpha = 0.01
+
+
+#%% determine statistically significant effects
+sl = pd.read_hdf(os.path.join(
+        helpers.resultsdir, basefile), 'second_level').loc[0]
+
+winnames = sl.index.get_level_values('time').unique()
+
+ss.add_measure(sl, 'mlog10p_fdr')
+
+sig = sl[sl[('mlog10p_fdr', r_name)] > -np.log10(fdr_alpha)].xs(
+        r_name, level='regressor', axis=1)
+
+srcdf = sl.xs(r_name, level='regressor', axis=1).copy()
+srcdf[srcdf['mlog10p_fdr'] < -np.log10(fdr_alpha)] = 0
+if show_measure not in srcdf.columns:
+    ss.add_measure(srcdf, show_measure)
     
-    twins = [("build-up", [-500, -120]),
-             ("response", [-30, 100])]
 
-winnames = [win[0] for win in twins]
+#%% define some plotting functions
+def get_colorinfo(srcdf, measure):
+    srcdf = srcdf[srcdf[measure] != 0]
+    
+    return {'fmin': srcdf[measure].abs().min(),
+            'fmid': srcdf[measure].abs().median(), 
+            'fmax': srcdf[measure].abs().max(), 
+            'transparent': True,
+            'colormap': 'auto'}
+    
+    
+def make_brain_figures(srcdf, measure, cinfo, filepat_base, top=5):
+    views = {'rh': ['medial', 'lateral'],
+             'lh': ['lateral', 'medial']}
+    
+    toplabels = {}
+    
+    for hemi in ['lh', 'rh']:
+        brain = Brain('fsaverage', hemi, 'inflated', cortex='low_contrast',
+                      subjects_dir=sv.subjects_dir, background='w', 
+                      foreground='k')
+            
+        sv.show_labels_as_data(srcdf, measure, brain, 
+                               time_label=None, parc=parc, **cinfo)
+        
+        if top:
+            ind = srcdf.index.get_level_values('label').map(
+                    lambda x: x.startswith(hemi[0].upper()))
+            
+            topl = srcdf[ind].abs().sort_values(
+                    measure, ascending=False).head(top)
+            topl = (
+                    topl[(topl[measure] != 0) 
+                              & topl[measure].notna()]
+                    .index.droplevel('time'))
+            toplabels[hemi] = topl
+            
+            alllabels = mne.read_labels_from_annot(
+                    'fsaverage', parc=parc, hemi=hemi)
+            
+            brain.remove_labels()
+            for label in alllabels:
+                if label.name in topl:
+                    brain.add_label(label, borders=1, hemi=hemi, alpha=0.8, 
+                                    color='k')
+        
+        # increase font size of colorbar - this only works by increasing the 
+        # colorbar itself and setting the ratio of colorbar to text
+        brain.data['colorbar'].scalar_bar.bar_ratio = 0.35
+        brain.data['colorbar'].scalar_bar_representation.position = [0.075, 0.01]
+        brain.data['colorbar'].scalar_bar_representation.position2 = [0.85, 0.12]
+        
+        filepat = os.path.join(figdir, filepat_base + '_{}.png'.format(hemi))
+        
+        brain.save_montage(filepat, views[hemi], colorbar=0)
+        
+        brain.close()
+        
+    infiles = [
+            os.path.join(figdir, filepat_base + '_{}.png'.format(hemi))
+            for hemi in ['lh', 'rh']]
+    outfile = os.path.join(figdir, filepat_base + '.png')
+    os.system("montage -tile 2x1 -geometry +0+0 {} {}".format(
+            ' '.join(infiles), outfile))
+    
+    return toplabels
 
-show_measure = 'tval'
+
+#%% show results in brain
+interactive = True
+
+toplstr = lambda ls: ', '.join([l[2:-7] for l in ls])
+
+if interactive:
+    hemi = 'lh'
+    wname = 'build-up'
+    
+    brain = Brain('fsaverage', hemi, 'inflated', cortex='low_contrast',
+                  subjects_dir=sv.subjects_dir, background='w', foreground='k')
+    
+    sv.show_labels_as_data(srcdf.loc[(slice(None), wname), :], show_measure, 
+                           brain, time_label=None, parc=parc,
+                           **get_colorinfo(srcdf, show_measure))
+else:
+    toplstrs = []
+    for wname in winnames:
+        filepat_base = 'win_brain_{}_{}_{}'.format(r_name, show_measure, wname)
+        
+        df = srcdf.loc[(slice(None), wname), :]
+        cinfo = get_colorinfo(df, show_measure)
+        toplabels = make_brain_figures(df, show_measure, cinfo, filepat_base)
+        
+        toplstrs.append('{}: left - {}; right - {}'.format(
+                wname, toplstr(toplabels['lh']), toplstr(toplabels['rh'])))
 
 
-#%% prepare data
+#%% investigate differences
+
+# whether to shift the magnitudes before scaling
+shift = True
+        
 data = pd.read_hdf(os.path.join(helpers.resultsdir, basefile), 'first_level')
 data = data.loc[0].xs('beta', level='measure', axis=1).xs(
         r_name, level='regressor', axis=1).copy()
 
 # scale data so that for each time point and subject the mean magnitude of
-# betas across brain areas is 1
-data = data.unstack('time')
-datanorm = data / data.abs().mean()
+# betas across brain areas with significant effects is 1
+magnitudes = data.unstack('time').abs()
+siglabels = sig.index.get_level_values('label').unique()
+#siglabels = magnitudes.index
+#siglabels = ['L_4_ROI-lh', 'R_4_ROI-rh']
+
+zero = magnitudes.iloc[0].copy()
+scale = magnitudes.iloc[0].copy()
+for wname in winnames:
+    sigl = sig.xs(wname, level='time').index
+    
+    _, zero.loc[(slice(None), wname)], scale.loc[(slice(None), wname)] = (
+            helpers.normalise_magnitudes(magnitudes.loc[
+                    sigl, (slice(None), wname)]))
+
+datanorm = (magnitudes - zero) / scale
+datanorm.loc[magnitudes.index.difference(siglabels)] = 0
 datanorm = datanorm.stack('time')
 
-# average within the time windows
-avdata = pd.concat(
-        [datanorm.loc[(slice(None), slice(*(win[1]))), :].groupby('label').mean() 
-         for win in twins], keys=winnames, 
-        names=['window', 'label'])
-
-# calculate magnitude(!) difference
-diff = avdata.loc[winnames[0]].abs() - avdata.loc[winnames[1]].abs()
+# calculate magnitude difference
+diff = (datanorm.xs(winnames[0], level='time')
+        - datanorm.xs(winnames[1], level='time'))
 
 # perform t-test
 tvals, pvals = scipy.stats.ttest_1samp(diff, 0, axis=1)
 
 # put results into standard container
-srcdf = pd.DataFrame(
+srcdfdiff = pd.DataFrame(
         np.c_[diff.mean(axis=1), diff.std(axis=1), tvals, pvals],
         index=pd.MultiIndex.from_product(
                 [diff.index, [0]],
                 names=['label', 'time']),
         columns=['mean', 'std', 'tval', 'pval'])
 
+# determine significant differences with FDR correction
+srcdfdiff['mlog10p'] = -np.log10(srcdfdiff['pval'])
+srcdfdiffsig = srcdfdiff.loc[list(siglabels), :].copy()
+ss.add_measure(srcdfdiffsig, 'mlog10p_fdr')
 
-#%% show results in brain
-if show_measure not in srcdf.columns:
-    ss.add_measure(srcdf, show_measure)
+# zero-out non-significant differences
+srcdfdiff.loc[srcdfdiffsig[
+        srcdfdiffsig['mlog10p_fdr'] < -np.log10(fdr_alpha)].index] = 0
 
-hemi = 'lh'
 
-brain = Brain('fsaverage', hemi, 'inflated', cortex='low_contrast',
-              subjects_dir=sv.subjects_dir, background='w', foreground='k')
-
-sv.show_labels_as_data(srcdf, show_measure, brain, time_label=None, parc=parc,
-                       center=0)
+#%% show in brain
+diff_show_measure = 'tval'
+if interactive:
+    if 'brain' not in locals():
+        hemi = 'lh'
+        brain = Brain(
+                'fsaverage', hemi, 'inflated', cortex='low_contrast',
+                 subjects_dir=sv.subjects_dir, background='w', foreground='k')
+    sv.show_labels_as_data(srcdfdiff, diff_show_measure, brain, 
+                           time_label=None, parc=parc, center=0);
+else:
+    filepat_base = 'windiff_brain_{}_{}_{}-{}'.format(
+            r_name, diff_show_measure, winnames[0], winnames[1])
+    cinfo = get_colorinfo(srcdfdiff, diff_show_measure)
+    cinfo['center'] = 0
+    make_brain_figures(srcdfdiff, diff_show_measure, cinfo, filepat_base, 
+                       top=0);
+                       
+print(', '.join(srcdfdiff[srcdfdiff['mean'] != 0].index
+      .get_level_values('label').map(lambda l: l[2:-7]).unique()))
